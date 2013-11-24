69379f0af924ac362f585602a1a1eda0929fc0f8
Howard Miller <howardsmiller@gmail.com>
2013-11-24 10:31:33 +0000

    theme_iomad: basic bootstrap version (with layout issues)

--- theme/iomad/layout/embedded.php
+++ theme/iomad/layout/embedded.php
@@ -14,12 +14,22 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
-require_once(dirname(__FILE__) . '/../../config.php');
-require_once($CFG->libdir . '/filelib.php');
-require_once("lib.php");
+echo $OUTPUT->doctype() ?>
+<html <?php echo $OUTPUT->htmlattributes(); ?>>
+<head>
+    <title><?php echo $OUTPUT->page_title(); ?></title>
+    <link rel="shortcut icon" href="<?php echo $OUTPUT->favicon(); ?>" />
+    <?php echo $OUTPUT->standard_head_html() ?>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
 
-header('Content-type: text/css');
-
-$css = file_get_contents("style/company.css");
-
-echo iomad_process_company_css($css, null);
+<body <?php echo $OUTPUT->body_attributes(); ?>>
+<?php echo $OUTPUT->standard_top_of_body_html() ?>
+<div id="page">
+    <div id="page-content" class="clearfix">
+        <?php echo $OUTPUT->main_content(); ?>
+    </div>
+</div>
+<?php echo $OUTPUT->standard_end_of_body_html() ?>
+</body>
+</html>
