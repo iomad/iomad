926e7f0ed1498194aedcf28cf6a7c7f757aa652b
Derick Turner <derick@e-learndesign.co.uk>
2014-03-12 17:24:44 +0000

    IOMAD: an unassigned user get an error when logging into the site.  Fixes #46

--- local/iomad/lib/iomad.php
+++ local/iomad/lib/iomad.php
@@ -32,11 +32,11 @@ class iomad {
             $companyid = $SESSION->currenteditingcompany;
         } else if (self::is_company_user()) {
             $companyid = company_user::companyid();
-        } else if (!has_capability('block/iomad_company_admin:edit_departments', $context)) {
-            print_error('There has been a configuration error, please contact the site administrator');
-        } else {
+        } else if (has_capability('block/iomad_company_admin:edit_departments', $context)) {
             redirect(new moodle_url('/local/iomad_dashboard/index.php'),
                                      get_string('pleaseselect', 'block_iomad_company_admin'));
+        } else {
+            $companyid = 0;
         }
         return $companyid;
     }
