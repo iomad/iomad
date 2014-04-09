10526826bd127f045759f6ed8a9c68384becb89d
Derick Turner <derick@e-learndesign.co.uk>
2014-04-09 14:13:13 +0100

    IOMAD: users who use self sign up to create accounts should be put into a defined default company and or role.  Fixes #64
    Conflicts:
    	local/iomad_signup/lang/en/local_iomad_signup.php
    	local/iomad_signup/lib.php
    	local/iomad_signup/settings.php

---
+++ local/iomad_signup/lang/en/local_iomad_signup.php
@@ -0,0 +1,23 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+$string['company'] = 'Company users is assigned to';
+$string['configcompany'] = 'This is the company that the user will be assigned to once they have completed the sign up process';
+$string['enable'] = 'Enable';
+$string['enable_help'] = 'New email authentication users will be given the Client Administrator role on creation when this is enabled';
+$string['pluginname'] = 'Iomad signup';
+$string['role'] = 'Role to be assigned';
+$string['configrole'] = 'This is the role the user will be given when they have completed the sign up process';
---
+++ local/iomad_signup/lib.php
@@ -0,0 +1,61 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+defined('MOODLE_INTERNAL') || die;
+require_once($CFG->dirroot.'/local/iomad/lib/company.php');
+
+/**
+ * Event handler for 'user_created'
+ * For 'email' authentication (only) add this user
+ * to the defined role and company.
+ */
+function local_iomad_signup_user_created($user) {
+    global $CFG, $DB;
+
+    // the plugin needs to be enabled
+    if (!$CFG->local_iomad_signup_enable) {
+        return true;
+    }
+
+    // If not 'email' auth then we are not interested
+    if ($user->auth != 'email') {
+        return true;
+    }
+
+    // Get context
+    $context = context_system::instance();
+
+    // Do we have a company to assign?
+    if (!empty($CFG->local_iomad_signup_company)) {
+        // Get company.
+        $company = new company($CFG->local_iomad_signup_company);
+
+        // assign the user to the company.
+        $company->assign_user_to_company($user->id);
+    }
+    
+    // Do we have a role to assign?
+    if (!empty($CFG->local_iomad_signup_role)) {
+        // Get role
+        if ($role = $DB->get_record('role', array('id' => $CFG->local_iomad_signup_role), '*', MUST_EXIST)) {
+
+            // assign the user to the role
+            role_assign($role->id, $user->id, $context->id);
+        }
+    }
+
+    return true;
+}
