3062273ef67eca45f9ef9f1d037114d83d7d587d
Justin Li <jli@arising.net>
2017-07-19 11:45:41 -0400

    Included iomad/lib/user.php file because when calling funciton add_course, company_user class may not be found in some situation.

--- local/iomad/lib/company.php
+++ local/iomad/lib/company.php
@@ -13,7 +13,7 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
-
+require_once($CFG->dirroot . '/local/iomad/lib/user.php');
 class company {
     public $id = 0;
 
