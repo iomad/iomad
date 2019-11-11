024705e114c71fbc4d34fbdd4ef403492efc3ccb
Derick Turner <derick@e-learndesign.co.uk>
2019-11-11 18:20:06 +0000

    IOMAD: 3.6 weekly release

--- mod/iomadcertificate/version.php
+++ mod/iomadcertificate/version.php
@@ -31,4 +31,4 @@ $plugin->cron      = 0; // Period for cron to check this module (secs)
 $plugin->component = 'mod_iomadcertificate';
 
 $plugin->maturity  = MATURITY_STABLE;
-$plugin->release  = '3.6.6 (Build: 20190909)'; // Human-friendly version name
+$plugin->release  = '3.6.7 (Build: 20191111)'; // Human-friendly version name
