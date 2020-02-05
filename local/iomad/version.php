fc802e9a118644fb5748a518e1660588e2ab7c32
Derick Turner <derick@e-learndesign.co.uk>
2020-02-05 22:12:08 +0000

    IOMAD: record value for previous email template in edit company. closes #1271

--- local/iomad/version.php
+++ local/iomad/version.php
@@ -15,6 +15,6 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 $plugin->component  = 'local_iomad';
-$plugin->version  = 2019030107;   // The (date) version of this plugin.
+$plugin->version  = 2019030108;   // The (date) version of this plugin.
 $plugin->release  = '3.7.4 (Build: 20200113)'; // Human-friendly version name
 $plugin->requires = 2019052000;   // Requires this Moodle version.
