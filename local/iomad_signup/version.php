f4d937e556bba32ad3ded3e29c90e43a20bf1b29
Derick Turner <derick@e-learndesign.co.uk>
2020-02-05 22:37:25 +0000

    IOMAD: 3.6 weekly release

--- local/iomad_signup/version.php
+++ local/iomad_signup/version.php
@@ -16,5 +16,5 @@
 
 $plugin->component  = 'local_iomad_signup';
 $plugin->version  = 2016090301;   // The (date) version of this plugin.
-$plugin->release  = '3.6.8 (Build: 20200113)'; // Human-friendly version name
+$plugin->release  = '3.6.8+ (Build: 20200130)'; // Human-friendly version name
 $plugin->requires = 2013111800;   // Requires this Moodle version.
