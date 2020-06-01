cb316da4c92f4f2e9e4eae469d8dc1bc35fc6baf
Derick Turner <derick@e-learndesign.co.uk>
2020-06-01 16:19:20 +0100

    IOMAD: namespace issues with 3.6 #1364

--- auth/iomadoidc/classes/iomadoidcclient.php
+++ auth/iomadoidc/classes/iomadoidcclient.php
@@ -132,7 +132,7 @@ class iomadoidcclient {
 
         // IOMAD
         require_once($CFG->dirroot . '/local/iomad/lib/company.php');
-        $companyid = iomad::get_my_companyid(context_system::instance(), false);
+        $companyid = \iomad::get_my_companyid(\context_system::instance(), false);
         if (!empty($companyid)) {
             $postfix = "_$companyid";
         } else {
