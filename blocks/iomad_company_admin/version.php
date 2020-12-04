417aad6cfdb265b9b74df81601c8e2cb1109c79a
Derick Turner <derick@e-learndesign.co.uk>
2020-12-04 16:01:30 +0000

    IOMAD: Split managecourses capability to also have manageallcourses. closes #1503

--- blocks/iomad_company_admin/version.php
+++ blocks/iomad_company_admin/version.php
@@ -16,7 +16,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2020050700;   // The (date) version of this plugin.
+$plugin->version  = 2020120400;   // The (date) version of this plugin.
 $plugin->requires = 2019052000;   // Requires this Moodle version.
 $plugin->release  = '3.7.8 (Build: 20200914)'; // Human-friendly version name
 $plugin->component  = 'block_iomad_company_admin';
