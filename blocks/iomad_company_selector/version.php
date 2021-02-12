c88892d0b56a4e035c80b83ee4d236be99c79542
Derick Turner <derick@e-learndesign.co.uk>
2021-02-12 20:23:20 +0000

    IOMAD: fix up missing author and copyright in packages and sort any pckages which have the wrong package type in the files.

--- blocks/iomad_company_selector/version.php
+++ blocks/iomad_company_selector/version.php
@@ -14,6 +14,13 @@
 // You should have received a copy of the GNU General Public License
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * @package   block_iomad_company_selector
+ * @copyright 2021 Derick Turner
+ * @author    Derick Turner
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
 $plugin->version = 2016083100;
 $plugin->requires = 2010021900;   // Requires this Moodle version.
 $plugin->release  = '3.6.10 (Build: 20200511)'; // Human-friendly version name
