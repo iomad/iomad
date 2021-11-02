32694f029960704184a562dd1df57f0e136b4681
Derick Turner <derick@e-learndesign.co.uk>
2021-11-02 14:58:17 +0000

    IOMAD: company manager for child company can only add courses belonging to it's parent company.  Capability added to override this.

--- blocks/iomad_company_admin/version.php
+++ blocks/iomad_company_admin/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2020120400;   // The (date) version of this plugin.
+$plugin->version  = 2021110200;   // The (date) version of this plugin.
 $plugin->requires = 2019052000;   // Requires this Moodle version.
 $plugin->release  = '3.7.8 (Build: 20200914)'; // Human-friendly version name
 $plugin->component  = 'block_iomad_company_admin';
