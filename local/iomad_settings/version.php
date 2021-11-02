da02ada8dbb0a681a7c366e78733067985439f1a
Derick Turner <derick@e-learndesign.co.uk>
2021-11-02 14:58:49 +0000

    IOMAD: added default delay to email sending to local_iomad_settings

--- local/iomad_settings/version.php
+++ local/iomad_settings/version.php
@@ -22,6 +22,6 @@
  */
 
 $plugin->component  = 'local_iomad_settings';
-$plugin->version  = 2019022701;   // The (date) version of this plugin.
+$plugin->version  = 2021110200;   // The (date) version of this plugin.
 $plugin->release  = '3.7.8 (Build: 20200914)'; // Human-friendly version name
 $plugin->requires = 2019052000;   // Requires this Moodle version.
