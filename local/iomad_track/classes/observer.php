71680efd9bbfa78acdb66bd0e92d784d498924bb
Derick Turner <derick@e-learndesign.co.uk>
2023-10-17 16:13:46 +0100

    IOMAD: adding soem leeway on the final grade updates for IOMAD reporting tables in case of race conditions between that and course completion.

--- local/iomad_track/classes/observer.php
+++ local/iomad_track/classes/observer.php
@@ -697,9 +697,15 @@ class observer {
         }
 
         // Check if there is already an entry for this.
-        if ($entry = $DB->get_record('local_iomad_track', array('userid' => $userid,
-                                                                'courseid' => $courseid,
-                                                                'timecompleted' => null))) {
+        if ($entry = $DB->get_record_sql("SELECT * FROM {local_iomad_track}
+                                          WHERE userid = :userid
+                                          AND courseid = :courseid
+                                          AND
+                                           (timecompleted IS NULL OR
+                                            timecompleted + 5 > :eventtime)",
+                                         array('userid' => $userid,
+                                               'courseid' => $courseid,
+                                               'eventtime' => $event->timecreated))) {
             // We already have an entry.  Remove it.
             $DB->set_field('local_iomad_track', 'finalscore', $graderec->finalgrade/$graderec->rawgrademax * 100, array('id' => $entry->id));
             $DB->set_field('local_iomad_track', 'modifiedtime', $event->timecreated, array('id' => $entry->id));
