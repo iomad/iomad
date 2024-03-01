2583a740ac028fa58d36205679ddbb5f6d21eff4
Derick Turner <derick@e-learndesign.co.uk>
2024-03-01 23:53:07 +0000

    IOMAD: errors processing training event reminders

--- mod/trainingevent/classes/task/send_reminder_emails.php
+++ mod/trainingevent/classes/task/send_reminder_emails.php
@@ -23,6 +23,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 namespace mod_trainingevent\task;
+use company;
+use EmailTemplate;
 
 defined('MOODLE_INTERNAL') || die();
 
@@ -49,47 +51,52 @@ class send_reminder_emails extends \core\task\scheduled_task {
         $runtime = time();
 
         // Get all of the training events which have reminders set and not already passed.
-        if ($trainingevents = $DB->get_records_sql("SELECT * FROM {trainingevent}
+        if ($trainingevents = $DB->get_records_sql("SELECT *
+                                                    FROM {trainingevent}
                                                     WHERE sendreminder > 0
-                                                    AND startdatetime < :now",
+                                                    AND setreminder = 1
+                                                    AND startdatetime > :now",
                                                     ['now' => $runtime])) {
             foreach ($trainingevents as $trainingevent) {
                 // Do we need to do anything?
-                if ((($trainingevent->sendreminder + 1 ) * 24 * 60 * 60 + $runtime) > $trainingevent->startdate &&
-                    ($trainingevent->sendreminder *24 * 60 *60 + $runtime) < $trainingevent->startdate ) {
-                        // Get all of the users for this event.
-                        $eventusers = $DB->get_records('trainingevent_users', ['trainingeventid' => $trainingevent->id, 'waitlisted' => 0]);
+                if ((($trainingevent->sendreminder) * 24 * 60 * 60 + $runtime > $trainingevent->startdatetime) &&
+                    $runtime < $trainingevent->startdatetime) {
 
-                        // Is anyone signed up?
-                        if (empty($eventusers)) {
-                            continue;
-                        }
+                    // Does the course actually exist?
+                    if (!$course = $DB->get_record('course', ['id' => $trainingevent->course])) {
+                        continue;
+                    }
 
-                        // Does the course actually exist?
-                        if (!$course = $DB->get_record('course', ['id' => $trainingevent->course])) {
-                            continue;
-                        }
+                    // How about the location?
+                    if (!$location = $DB->get_record('classroom', array('id' => $trainingevent->classroomid))) {
+                        continue;
+                    }
 
-                        // How about the location?
-                        if (!$location = $DB->get_record('classroom', array('id' => $event->classroomid))) {
-                            continue;
-                        }
-                        $location->time = date($CFG->iomad_date_format . ' \a\t H:i', $event->startdatetime);
+                    // Set the company up for the emails.
+                    $company = new company($location->companyid);
 
-                        // Send the reminders.
-                        foreach ($eventusers as $eventuser) {
-                            if ($user = $DB->get_record('user', ['id' => $eventuser->userid, 'suspended' => 0, 'deleted' => 0])) {
-                                // Get the user's company.
-                                $usercompany = \company::by_userid($user->id);
-                                EmailTemplate::send('user_signed_up_for_event_reminder', array('course' => $course,
-                                                                                               'user' => $USER,
-                                                                                               'classroom' => $location,
-                                                                                               'company' => $usercompany,
-                                                                                               'event' => $event));
+                    // Get all of the users for this event.
+                    $eventusers = $DB->get_records('trainingevent_users', ['trainingeventid' => $trainingevent->id, 'waitlisted' => 0]);
 
-                            }
+                    // Is anyone signed up?
+                    if (empty($eventusers)) {
+                        continue;
+                    }
+
+                    $location->time = date($CFG->iomad_date_format . ' \a\t H:i', $trainingevent->startdatetime);
+
+                    // Send the reminders.
+                    foreach ($eventusers as $eventuser) {
+                        if ($user = $DB->get_record('user', ['id' => $eventuser->userid, 'suspended' => 0, 'deleted' => 0])) {
+                            EmailTemplate::send('user_signed_up_for_event_reminder', array('course' => $course,
+                                                                                           'user' => $user,
+                                                                                           'classroom' => $location,
+                                                                                           'company' => $company,
+                                                                                           'event' => $trainingevent));
+
                         }
                     }
+                }
             }
         }
     }
