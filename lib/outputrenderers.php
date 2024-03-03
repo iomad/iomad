moodle/moodle

IOMAD: 4.3 issues on install over Moodle due to company context (and others) not having the company table set up before they did the initial checks.

lib/outputrenderers.php
@@ -3908,18 +3908,18 @@ EOD;
             $companycontext = $systemcontext;
         }
 
-        if (\iomad::has_capability('block/iomad_company_admin:companymanagement_view', $companycontext) ||
-            \iomad::has_capability('block/iomad_company_admin:usermanagement_view', $companycontext) ||
-            \iomad::has_capability('block/iomad_company_admin:coursemanagement_view', $companycontext) ||
-            \iomad::has_capability('block/iomad_company_admin:licensemanagement_view', $companycontext) ||
-            \iomad::has_capability('block/iomad_company_admin:competencymanagement_view', $companycontext) ||
-            \iomad::has_capability('block/iomad_commerce:admin_view', $companycontext) ||
-            \iomad::has_capability('block/iomad_microlearning:view', $companycontext) ||
-            \iomad::has_capability('block/iomad_reports:view', $companycontext)) {
+        $iomadlink = "";
+        if ($DB->get_manager()->table_exists('company') &&
+            (\iomad::has_capability('block/iomad_company_admin:companymanagement_view', $companycontext) ||
+             \iomad::has_capability('block/iomad_company_admin:usermanagement_view', $companycontext) ||
+             \iomad::has_capability('block/iomad_company_admin:coursemanagement_view', $companycontext) ||
+             \iomad::has_capability('block/iomad_company_admin:licensemanagement_view', $companycontext) ||
+             \iomad::has_capability('block/iomad_company_admin:competencymanagement_view', $companycontext) ||
+             \iomad::has_capability('block/iomad_commerce:admin_view', $companycontext) ||
+             \iomad::has_capability('block/iomad_microlearning:view', $companycontext) ||
+             \iomad::has_capability('block/iomad_reports:view', $companycontext))) {
             $iomadlink = "-" . get_string('dashboard', 'block_iomad_company_admin') . "|" .
                          '/blocks/iomad_company_admin/index.php' . "\n\r";
-        } else {
-            $iomadlink = "";
         }
 
         // Deal with company custom and shop menu items.
@@ -3927,7 +3927,8 @@ EOD;
 
         // Deal with company custom menu items.
         if ($companyid = \iomad::get_my_companyid(\context_system::instance(), false)) {
-            if ($companyrec = $DB->get_record('company', array('id' => $companyid))) {
+            if ($DB->get_manager()->table_exists('company') &&
+                $companyrec = $DB->get_record('company', array('id' => $companyid))) {
                 if (!empty($companyrec->custommenuitems)) {
                     $custommenuitems = $companyrec->custommenuitems;
                 }

iomad/iomad

lib/outputrenderers.php
@@ -3823,24 +3823,25 @@ EOD;
 
         // IOAMD
         $systemcontext = \context_system::instance();
-        if (\iomad::has_capability('block/iomad_company_admin:companymanagement_view', $systemcontext) ||
-            \iomad::has_capability('block/iomad_company_admin:usermanagement_view', $systemcontext) ||
-            \iomad::has_capability('block/iomad_company_admin:coursemanagement_view', $systemcontext) ||
-            \iomad::has_capability('block/iomad_company_admin:licensemanagement_view', $systemcontext) ||
-            \iomad::has_capability('block/iomad_company_admin:competencymanagement_view', $systemcontext) ||
-            \iomad::has_capability('block/iomad_commerce:admin_view', $systemcontext) ||
-            \iomad::has_capability('block/iomad_microlearning:view', $systemcontext) ||
-            \iomad::has_capability('block/iomad_reports:view', $systemcontext)) {
+        $iomadlink = "";
+        if ($DB->get_manager()->table_exists('company') &&
+            (\iomad::has_capability('block/iomad_company_admin:companymanagement_view', $systemcontext) ||
+             \iomad::has_capability('block/iomad_company_admin:usermanagement_view', $systemcontext) ||
+             \iomad::has_capability('block/iomad_company_admin:coursemanagement_view', $systemcontext) ||
+             \iomad::has_capability('block/iomad_company_admin:licensemanagement_view', $systemcontext) ||
+             \iomad::has_capability('block/iomad_company_admin:competencymanagement_view', $systemcontext) ||
+             \iomad::has_capability('block/iomad_commerce:admin_view', $systemcontext) ||
+             \iomad::has_capability('block/iomad_microlearning:view', $systemcontext) ||
+             \iomad::has_capability('block/iomad_reports:view', $systemcontext))) {
             $iomadlink = "-" . get_string('dashboard', 'block_iomad_company_admin') . "|" .
                          '/blocks/iomad_company_admin/index.php' . "\n\r";
-        } else {
-            $iomadlink = "";
         }
 
         // Deal with company custom and shop menu items.
         $shoplink = "";
         if ($companyid = \iomad::get_my_companyid(\context_system::instance(), false)) {
-            if ($companyrec = $DB->get_record('company', array('id' => $companyid))) {
+            if ($DB->get_manager()->table_exists('company') &&
+                $companyrec = $DB->get_record('company', array('id' => $companyid))) {
                 if (!empty($companyrec->custommenuitems)) {
                     $custommenuitems = $companyrec->custommenuitems;
                 }