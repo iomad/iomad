7798b6b8feb177c76279b59b596dc5a971515bf9
Derick Turner <derick@e-learndesign.co.uk>
2024-03-07 10:36:09 +0000

    IOMAD: old repeateday field in email_templateset_templates didn't get renamed

--- local/email/version.php
+++ local/email/version.php
@@ -22,6 +22,6 @@
  */
 
 $plugin->release  = '4.1.9 (Build: 20240212)'; // Human-friendly version name
-$plugin->version  = 2023030900;   // The (date) version of this plugin.
+$plugin->version  = 2024030700;   // The (date) version of this plugin.
 $plugin->requires = 2019052000;   // Requires this Moodle version.
 $plugin->component  = 'local_email';
