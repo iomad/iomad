789c0dac955f66bfea69331f8887048643e590e0
Derick Turner <derick@e-learndesign.co.uk>
2024-06-10 11:46:51 +0100

    IOMAD: 4.1.11 release

--- local/iomad/version.php
+++ local/iomad/version.php
@@ -21,7 +21,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->release  = '4.1.10 (Build: 20240422)'; // Human-friendly version name
+$plugin->release  = '4.1.11 (Build: 20240610)'; // Human-friendly version name
 $plugin->component  = 'local_iomad';
 $plugin->requires = 2019052000;   // Requires this Moodle version.
 $plugin->version  = 2023042700;   // The (date) version of this plugin.
