a03f950399c1b158c9b1c1bc9cc9527bf992352b
Derick Turner <derick@e-learndesign.co.uk>
2024-08-30 15:10:34 +0100

    IOMAD: added training event reminder sent field to stop too many reminders

--- mod/trainingevent/db/upgrade.php
+++ mod/trainingevent/db/upgrade.php
@@ -201,34 +201,6 @@ function xmldb_trainingevent_upgrade($oldversion) {
         upgrade_mod_savepoint(true, 2024030100, 'trainingevent');
     }
 
-    if ($oldversion < 2024083000) {
-
-        // Define field remindersent to be added to trainingevent.
-        $table = new xmldb_table('trainingevent');
-        $field = new xmldb_field('remindersent', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0', 'isexclusive');
-
-        // Conditionally launch add field remindersent.
-        if (!$dbman->field_exists($table, $field)) {
-            $dbman->add_field($table, $field);
-        }
-
-        // Update all of the past events and events which will already have sent a reminder.
-        $runtime = time();
-        $DB->set_field_select('trainingevent',
-                              'remindersent',
-                               1,
-                              "setreminder = 1 AND startdatetime < :runtime",
-                              ['runtime' => $runtime]);
-        $DB->set_field_select('trainingevent',
-                              'remindersent',
-                               1,
-                              "setreminder = 1 AND sendreminder > 0 AND (sendreminder - 1) *24 * 60 * 60 + :runtime > startdatetime",
-                              ['runtime' => $runtime]);
-
-        // Trainingevent savepoint reached.
-        upgrade_mod_savepoint(true, 2024083000, 'trainingevent');
-    }
-
     if ($oldversion < 2025010600) {
 
         // Define field booking_notes to be added to trainingevent_users.
@@ -281,6 +253,28 @@ function xmldb_trainingevent_upgrade($oldversion) {
     }
 
     if ($oldversion < 2025012300) {
+
+        // Define field remindersent to be added to trainingevent.
+        $table = new xmldb_table('trainingevent');
+        $field = new xmldb_field('remindersent', XMLDB_TYPE_INTEGER, '1', null, XMLDB_NOTNULL, null, '0', 'isexclusive');
+
+        // Conditionally launch add field remindersent.
+        if (!$dbman->field_exists($table, $field)) {
+            $dbman->add_field($table, $field);
+        }
+
+        // Update all of the past events and events which will already have sent a reminder.
+        $runtime = time();
+        $DB->set_field_select('trainingevent',
+                              'remindersent',
+                               1,
+                              "setreminder = 1 AND startdatetime < :runtime",
+                              ['runtime' => $runtime]);
+        $DB->set_field_select('trainingevent',
+                              'remindersent',
+                               1,
+                              "setreminder = 1 AND sendreminder > 0 AND (sendreminder - 1) *24 * 60 * 60 + :runtime > startdatetime",
+                              ['runtime' => $runtime]);
 
         // Define field requirenotes to be added to trainingevent.
         $table = new xmldb_table('trainingevent');
