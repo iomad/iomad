f260854174b52cfdd4c1422fa6c1dcad6dcba66f
Derick Turner <derick@e-learndesign.co.uk>
2024-09-02 09:51:53 +0100

    IOMAD: 4.1.13 release

--- blocks/iomad_commerce/version.php
+++ blocks/iomad_commerce/version.php
@@ -23,7 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release  = '4.1.12 (Build: 20240812)'; // Human-friendly version name
+$plugin->release  = '4.1.13 (Build: 20240902)'; // Human-friendly version name
 $plugin->version  = 2023090500;   // The (date) version of this plugin.
 $plugin->requires = 2019052000;   // Requires this Moodle version.
 $plugin->component = 'block_iomad_commerce';
--- blocks/iomad_learningpath/version.php
+++ blocks/iomad_learningpath/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release  = '4.1.12 (Build: 20240812)'; // Human-friendly version name
+$plugin->release  = '4.1.13 (Build: 20240902)'; // Human-friendly version name
 $plugin->version  = 2024082700;
 $plugin->requires = 2019052000;
 $plugin->component  = 'block_iomad_learningpath';
