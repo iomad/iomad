1468d2f1c8a877e16e13a7ad09854a6887e265e7
Derick Turner <derick@e-learndesign.co.uk>
2025-02-07 10:37:56 +0000

    IOMAD: Course completion report - invalid state when regenerating a single certificate - closes #2201

--- local/report_completion/index.php
+++ local/report_completion/index.php
@@ -494,6 +494,7 @@ if (!empty($action)) {
                     // We are only removing the saved record for this.
                     echo $OUTPUT->confirm(get_string('purgerecordconfirm', 'local_report_users'), $confirmurl, $cancel);
                 }
+                echo $OUTPUT->footer();
                 die;
             } else {
                 $confirmurl = new moodle_url('/local/report_completion/index.php',
@@ -508,6 +509,8 @@ if (!empty($action)) {
                                              ));
                 $cancel = new moodle_url('/local/report_completion/index.php', $params);
                 echo $OUTPUT->confirm(get_string('redocertificateconfirm', 'local_report_users'), $confirmurl, $cancel);
+                echo $OUTPUT->footer();
+                die;
             }
         }
     }
