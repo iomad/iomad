68fee9d193e62f79ef69126a7ae2c6f2dd5ced06
Derick Turner <derick@e-learndesign.co.uk>
2025-02-10 23:43:09 +0000

    IOMAD: new core MFA factor SMS added 4.4+ - closes #2215

--- admin/tool/mfa/factor/sms/settings.php
+++ admin/tool/mfa/factor/sms/settings.php
@@ -26,27 +26,36 @@
 defined('MOODLE_INTERNAL') || die();
 global $CFG, $OUTPUT;
 
+// IOMAD
+require_once($CFG->dirroot . '/local/iomad/lib/company.php');
+$postfix = "";
+$companyid = iomad::get_my_companyid(context_system::instance(), false);
+if (!empty($companyid)) {
+    $postfix = "_$companyid";
+}
+
 if ($ADMIN->fulltree) {
-    $enabled = new admin_setting_configcheckbox('factor_sms/enabled',
+    $enabled = new admin_setting_configcheckbox('factor_sms/enabled' . $postfix,
         new lang_string('settings:enablefactor', 'tool_mfa'),
         new lang_string('settings:enablefactor_help', 'tool_mfa'), 0);
     $enabled->set_updatedcallback(function () {
-        \tool_mfa\manager::do_factor_action('sms', get_config('factor_sms', 'enabled') ? 'enable' : 'disable');
+        global $postfix;
+        \tool_mfa\manager::do_factor_action('sms', get_config('factor_sms', 'enabled' . $postfix) ? 'enable' : 'disable');
     });
     $settings->add($enabled);
 
-    $settings->add(new admin_setting_configtext('factor_sms/weight',
+    $settings->add(new admin_setting_configtext('factor_sms/weight' . $postfix,
         new lang_string('settings:weight', 'tool_mfa'),
         new lang_string('settings:weight_help', 'tool_mfa'), 100, PARAM_INT));
 
-    $settings->add(new admin_setting_configduration('factor_sms/duration',
+    $settings->add(new admin_setting_configduration('factor_sms/duration' . $postfix,
         get_string('settings:duration', 'tool_mfa'),
         get_string('settings:duration_help', 'tool_mfa'), 30 * MINSECS, MINSECS));
 
     $codeslink = 'https://en.wikipedia.org/wiki/List_of_country_calling_codes';
     $link = \html_writer::link($codeslink, $codeslink);
 
-    $settings->add(new admin_setting_configtext('factor_sms/countrycode',
+    $settings->add(new admin_setting_configtext('factor_sms/countrycode' . $postfix,
         get_string('settings:countrycode', 'factor_sms'),
         get_string('settings:countrycode_help', 'factor_sms', $link), '', PARAM_INT));
 
@@ -54,7 +63,7 @@ if ($ADMIN->fulltree) {
         'aws_sns' => get_string('settings:aws', 'factor_sms'),
     ];
 
-    $settings->add(new admin_setting_configselect('factor_sms/gateway',
+    $settings->add(new admin_setting_configselect('factor_sms/gateway' . $postfix,
         get_string('settings:gateway', 'factor_sms'),
         get_string('settings:gateway_help', 'factor_sms'),
         'aws_sns', $gateways));
@@ -63,6 +72,6 @@ if ($ADMIN->fulltree) {
         return;
     }
 
-    $class = '\factor_sms\local\smsgateway\\' . get_config('factor_sms', 'gateway');
+    $class = '\factor_sms\local\smsgateway\\' . get_config('factor_sms', 'gateway' . $postfix);
     call_user_func($class . '::add_settings', $settings);
 }
