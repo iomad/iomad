35bbc6f2012901a3a6375623ccd1fd3080b93081
Derick Turner <derick@e-learndesign.co.uk>
2025-03-17 11:21:18 +0000

    IOMAD: 4.3.11 release

--- availability/condition/trainingevent/version.php
+++ availability/condition/trainingevent/version.php
@@ -24,7 +24,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release  = '4.3.10 (Build: 20250210)'; // Human-friendly version name
+$plugin->release  = '4.3.11 (Build: 20250317)'; // Human-friendly version name
 $plugin->version = 2022041900;
 $plugin->requires = 2022041200;
 $plugin->component = 'availability_trainingevent';
