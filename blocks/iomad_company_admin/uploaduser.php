fe6edcb07e88facd24c14f9b285655da39e0de1f
Derick Turner <derick@e-learndesign.co.uk>
2025-04-04 13:11:23 +0100

    IOMAD: Adding a text area optional profile field causes issues with bulk user upload. - closes #2273

--- blocks/iomad_company_admin/uploaduser.php
+++ blocks/iomad_company_admin/uploaduser.php
@@ -474,7 +474,7 @@ if ($mform->is_cancelled()) {
                 }
             }
             foreach ($prffields as $field) {
-                if (isset($user->$field)) {
+                if (isset($user->$field) && is_string($user->$field)) {
                     if (preg_match('/(?P<day>\d{2})-(?P<month>[a-zA-Z]{3})-(?P<year>\d{4})/', $user->$field, $datearray)) {
                         $month = $montharray[$datearray[2]];
                         $unixtime = mktime (0, 0, 0, $month, $datearray['day'], $datearray['year']);
@@ -482,7 +482,7 @@ if ($mform->is_cancelled()) {
                     }
                     continue;
                 }
-                if (isset($formdata->$field)) {
+                if (isset($formdata->$field) && is_string($user->$field)) {
                     // Process templates.
                     // Check if is in a dd-Mon-yyy format.
                     if (preg_match('/(?P<day>\d{2})-(?P<month>[a-zA-Z]{3})-(?P<year>\d{4})/', $formdata->$field, $datearray)) {
