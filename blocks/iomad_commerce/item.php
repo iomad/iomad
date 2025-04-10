47befb214e285af41d042f962f91e8d0bdc2ee50
Robert <robert@e-learndesign.co.uk>
2025-04-10 11:32:46 +0100

    IOMAD: remove php warning by adding isset() to if statement in blocks/iomad_commerce/item.php

--- blocks/iomad_commerce/item.php
+++ blocks/iomad_commerce/item.php
@@ -68,7 +68,7 @@ if ($item) {
 
     echo '<h3>' . format_string($item->name) . "</h3>";
 
-    if ($item->long_description) {
+    if (isset($item->long_description)) {
         echo $item->long_description;
     } else {
         echo $item->summary;
