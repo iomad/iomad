87b2f134d4f6cacd7b6bee45c2723c708d75fe05
Derick Turner <derick@e-learndesign.co.uk>
2025-04-18 15:55:06 +0100

    IOMAD: added push date to iomad version.php so we can track better.  This will be the date we last pushed to public repositories

--- local/iomad/version.php
+++ local/iomad/version.php
@@ -19,6 +19,7 @@
  * @copyright 2021 Derick Turner
  * @author    Derick Turner
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @push date 2025/04/18
  */
 
 $plugin->release  = '4.5.3 (Build: 20250317)'; // Human-friendly version name
