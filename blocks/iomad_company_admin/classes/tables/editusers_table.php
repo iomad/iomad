8b2f9acc6de47a9f46e4d655b1c00708024d4b63
Robert <robert@e-learndesign.co.uk>
2025-04-21 11:30:23 +0100

    IOMAD: correct comment

--- blocks/iomad_company_admin/classes/tables/editusers_table.php
+++ blocks/iomad_company_admin/classes/tables/editusers_table.php
@@ -179,7 +179,7 @@ class editusers_table extends table_sql {
                 $currentvalue = 0;
             }
 
-            // Output text if the department doesn't match the top level department
+            // Output text if the department doesn't match the currently selected department
             if (count($userdepartments) > 1 ||
                 isset($userdepartments[0]) && $userdepartments[0] != $this->parentlevel->id && (!isset($params['deptid']) || $userdepartments[0] != $params['deptid'])) {
                 return $usertypeselect[$currentvalue];
