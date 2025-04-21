2f5e9429d5162ee4d4257d6748de47a73e9c8613
Robert <robert@e-learndesign.co.uk>
2025-04-21 11:23:49 +0100

    IOMAD: Prevent Warning: Undefined array key 'deptid' when it is not defined

--- blocks/iomad_company_admin/classes/tables/editusers_table.php
+++ blocks/iomad_company_admin/classes/tables/editusers_table.php
@@ -181,7 +181,7 @@ class editusers_table extends table_sql {
 
             // Output text if the department doesn't match the top level department
             if (count($userdepartments) > 1 ||
-                isset($userdepartments[0]) && $userdepartments[0] != $this->parentlevel->id && $userdepartments[0] != $params['deptid']) {
+                isset($userdepartments[0]) && $userdepartments[0] != $this->parentlevel->id && (!isset($params['deptid']) || $userdepartments[0] != $params['deptid'])) {
                 return $usertypeselect[$currentvalue];
             }
 
