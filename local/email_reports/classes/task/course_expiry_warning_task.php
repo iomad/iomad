273d67996a6da4dbfce67424d67a2f6df7efdf3f
Derick Turner <derick@e-learndesign.co.uk>
2025-05-28 11:36:14 +0100

    IOMAD: local_email_reports Expiry warning task unsetting change to course cleared flag in the database closes #2335

--- local/email_reports/classes/task/course_expiry_warning_task.php
+++ local/email_reports/classes/task/course_expiry_warning_task.php
@@ -120,6 +120,9 @@ class course_expiry_warning_task extends \core\task\scheduled_task {
                                                                                             'userid' => $user->id));
                 $event->trigger();
 
+                // Set the local record object to match.
+                $compuser->coursecleared = 1;
+
                 // Get the company template info.
                 // Check against per company template repeat instead.
                 if ($templateinfo = $DB->get_record('email_template', array('companyid' => $company->id, 'lang' => $user->lang, 'name' => 'expiry_warn_user'))) {
