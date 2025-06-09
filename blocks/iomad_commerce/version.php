56d8408a868cf1810444d1053f7d5736e83f42bb
Derick Turner <derick@e-learndesign.co.uk>
2025-06-09 16:16:48 +0100

    IOMAD: version bump for this - #2337

--- blocks/iomad_commerce/version.php
+++ blocks/iomad_commerce/version.php
@@ -24,6 +24,6 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->release  = '4.1.18 (Build: 20250414)'; // Human-friendly version name
-$plugin->version  = 2025060900;   // The (date) version of this plugin.
+$plugin->version  = 2025060901;   // The (date) version of this plugin.
 $plugin->requires = 2019052000;   // Requires this Moodle version.
 $plugin->component = 'block_iomad_commerce';
