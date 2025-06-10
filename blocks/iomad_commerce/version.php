8abc85285382e8da5e968be4dc8dae25b3f89d49
Derick Turner <derick@e-learndesign.co.uk>
2025-06-10 09:57:20 +0100

    IOMAD: still issues with version mismatch

--- blocks/iomad_commerce/version.php
+++ blocks/iomad_commerce/version.php
@@ -23,6 +23,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version  = 2025060901;   // The (date) version of this plugin.
+$plugin->release  = '4.3.12 (Build: 20250414)'; // Human-friendly version name
+$plugin->version  = 2025061000;   // The (date) version of this plugin.
 $plugin->requires = 2019052000;   // Requires this Moodle version.
 $plugin->component = 'block_iomad_commerce';
