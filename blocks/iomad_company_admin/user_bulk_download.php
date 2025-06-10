f8b2224997b755c5b08183abc4459a47a5751231
Robert <robert@e-learndesign.co.uk>
2025-06-10 15:51:43 +0100

    IOMAD: Fix download for csv and xls formats for the bulk user download to not throw a error when it downloads users which have the custom profile field text area

--- blocks/iomad_company_admin/user_bulk_download.php
+++ blocks/iomad_company_admin/user_bulk_download.php
@@ -262,7 +262,11 @@ function user_download_xls($userids, $fields, $includecompanyfield) {
             set_time_limit(30);
             if ($includecompanyfield || $field != "profile_field_company") {
                 if (!empty($user->$field)) {
-                    $worksheet[0]->write($row, $col, $user->$field);
+                    if (isset($user->$field['text'])){
+                        $worksheet[0]->write($row, $col, $user->$field['text']);
+                    } else {
+                        $worksheet[0]->write($row, $col, $user->$field);
+                    }
                 } else {
                     $worksheet[0]->write($row, $col, '');
                 }
@@ -316,7 +320,11 @@ function user_download_csv($userids, $fields, $includecompanyfield) {
             set_time_limit(30);
             if ($includecompanyfield || $field != "profile_field_company") {
                 if (!empty($user->$field)) {
-                    $row[] = str_replace($delimiter, $encdelim, $user->$field);
+                    if(isset($user->$field['text'])){
+                        $row[] = str_replace($delimiter, $encdelim, $user->$field['text']);
+                    } else {
+                        $row[] = str_replace($delimiter, $encdelim, $user->$field);
+                    }
                 } else {
                     $row[] = str_replace($delimiter, $encdelim, '');
                 }
