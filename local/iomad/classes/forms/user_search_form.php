f0c88f850336b1940ca57d83dba611ae0cb1abba
Robert <robert@e-learndesign.co.uk>
2025-06-19 10:14:25 +0100

    IOMAD: fix textarea in editusers.php so it now filters the users when data is inputted to the field in the form - closes #2360

--- local/iomad/classes/forms/user_search_form.php
+++ local/iomad/classes/forms/user_search_form.php
@@ -152,6 +152,7 @@ class user_search_form extends moodleform {
             if ($fields = $DB->get_records('user_info_field', array('categoryid' => $category->id))) {
                 // Display the header and the fields.
                 foreach ($fields as $field) {
+                    $field->datatype = ($field->datatype == 'textarea') ? 'text' : $field->datatype;
                     require_once($CFG->dirroot.'/user/profile/field/'.$field->datatype.'/field.class.php');
                     $newfield = 'profile_field_'.$field->datatype;
                     $formfield = new $newfield($field->id);
@@ -172,6 +173,7 @@ class user_search_form extends moodleform {
                 if ($fields = $DB->get_records('user_info_field', array('categoryid' => $category->id))) {
                     // Display the header and the fields.
                     foreach ($fields as $field) {
+                        $field->datatype = ($field->datatype == 'textarea') ? 'text' : $field->datatype;
                         require_once($CFG->dirroot.'/user/profile/field/'.$field->datatype.'/field.class.php');
                         $newfield = 'profile_field_'.$field->datatype;
                         $formfield = new $newfield($field->id);
