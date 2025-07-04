e4b4f8ce3d40ab46f60ee71163e6e41dd389476c
Derick Turner <derick@e-learndesign.co.uk>
2025-07-04 17:03:48 +0100

    IOMAD: updated push date

--- local/iomad/version.php
+++ local/iomad/version.php
@@ -19,7 +19,7 @@
  * @copyright 2021 Derick Turner
  * @author    Derick Turner
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
- * @push date 2025/07/03
+ * @push date 2025/07/04
  */
 
 $plugin->release  = '4.4.9 (Build: 20250609)'; // Human-friendly version name
