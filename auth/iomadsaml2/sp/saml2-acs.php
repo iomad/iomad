a782502026e68296d588f50af4648e73a80fed84
Derick Turner <derick@e-learndesign.co.uk>
2025-07-17 09:58:08 +0100

    IOMAD: another reference which needed changing to iomadsaml2 - #2401

--- auth/iomadsaml2/sp/saml2-acs.php
+++ auth/iomadsaml2/sp/saml2-acs.php
@@ -37,6 +37,6 @@ try {
     $acs = $controller->assertionConsumerService($iomadsam2auth->spname);
     $acs->sendContent();
 } catch (Exception $e) {
-    throw new saml2_exception($e->getMessage(), $e->getTraceAsString());
+    throw new iomadsaml2_exception($e->getMessage(), $e->getTraceAsString());
 }
 
