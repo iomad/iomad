8e34a630306473b77d833efda519c1cf37dd00fd
Derick Turner <derick@e-learndesign.co.uk>
2025-07-21 17:13:32 +0100

    IOMAD: Unenrol user from course does not work anymore for company managers. issue was brought in in fix for #2310.  The wrong role id is being passed - should be ->roleid and not ->id Closes #2403

--- local/iomad/lib/user.php
+++ local/iomad/lib/user.php
@@ -566,7 +566,7 @@ class company_user {
                 if (!$all && $role->roleid == $studentrole->id) {
                     $isstudent = true;
                 } else {
-                    role_unassign($role->id, $user->id, $coursecontext->id);
+                    role_unassign($role->roleid, $user->id, $coursecontext->id);
                 }
             }
             if (!$isstudent) {
@@ -612,7 +612,7 @@ class company_user {
                     if (!$all && $role->roleid == $studentrole->id) {
                         $isstudent = true;
                     } else {
-                        role_unassign($role->id, $user->id, $coursecontext->id);
+                        role_unassign($role->roleid, $user->id, $coursecontext->id);
                     }
                 }
                 if (!$isstudent) {
