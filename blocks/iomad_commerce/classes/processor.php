5266dbd238a5da8fa4365ae003713c706686470d
Robert <robert@e-learndesign.co.uk>
2025-07-23 11:46:22 +0100

    IOMAD: Enrol a user into the course instead of using a license for single purchase. Update code to create a license for all courses when bulk purchasing (learning path shop item)

--- blocks/iomad_commerce/classes/processor.php
+++ blocks/iomad_commerce/classes/processor.php
@@ -140,39 +140,52 @@ class processor {
                 // Paths are included in the shop item
                 $totalcourses = 0;
                 $pathcoursesarray = [];
+                $pathcourseenrol[] = $pathcourse->course;
                 foreach ($paths as $path) {
                     $pathcourses = $DB->get_records('iomad_learningpathcourse',['path' => $path->pathid]);
                     foreach ($pathcourses as $pathcourse) {
                         if ($DB->get_record('iomad_courses', ['courseid' => $pathcourse->course, 'licensed' => 1])) {
                             $pathcoursesarray[] = $pathcourse->course;
                             $totalcourses++;
+                        } else {
+                            $pathcourseenrol[] = $pathcourse->course;
                         }
                     }
                 }
-                $companylicense->allocation = $totalcourses;
-                $companylicense->humanallocation = 1;
-                $companylicense->program = 1;
-                $companylicense->validlength = (!empty($iteminfo->single_purchase_validlength)) ? $iteminfo->single_purchase_validlength / 86400 : 1825;
-                $companylicenseid = $DB->insert_record('companylicense', $companylicense);
-                // Add the courses to the license
-                foreach ($pathcoursesarray as $pathcourse) {
-                    $DB->insert_record('companylicense_courses', ['licenseid' => $companylicenseid, 'courseid' => $pathcourse]);
-                    $licenseuserid = $DB->insert_record('companylicense_users', (object)['licenseid' => $companylicenseid, 
-                                                                                        'userid' => $invoice->userid,
-                                                                                        'isusing' => 0,
-                                                                                        'licensecourseid' => $pathcourse,
-                                                                                        'issuedate' => $runtime,
-                                                                                        'groupid' => 0]);
-                    // Create an event to assign the license.
-                    $eventother = array('licenseid' => $companylicenseid,
-                                        'issuedate' => $runtime,
-                                        'duedate' => $runtime);
-                    $event = \block_iomad_company_admin\event\user_license_assigned::create(array('context' => \context_course::instance($pathcourse),
-                                                                                                'objectid' => $licenseuserid,
-                                                                                                'courseid' => $pathcourse,
-                                                                                                'userid' => $invoice->userid,
-                                                                                                'other' => $eventother));
-                    $event->trigger();
+                if (!empty($pathcoursesarray)) {
+                    $companylicense->allocation = $totalcourses;
+                    $companylicense->humanallocation = 1;
+                    $companylicense->program = 1;
+                    $companylicense->validlength = (!empty($iteminfo->single_purchase_validlength)) ? $iteminfo->single_purchase_validlength / 86400 : 1825;
+                    $companylicenseid = $DB->insert_record('companylicense', $companylicense);
+                    // Add the courses to the license
+                    foreach ($pathcoursesarray as $pathcourse) {
+                        $DB->insert_record('companylicense_courses', ['licenseid' => $companylicenseid, 'courseid' => $pathcourse]);
+                        $licenseuserid = $DB->insert_record('companylicense_users', (object)['licenseid' => $companylicenseid, 
+                                                                                            'userid' => $invoice->userid,
+                                                                                            'isusing' => 0,
+                                                                                            'licensecourseid' => $pathcourse,
+                                                                                            'issuedate' => $runtime,
+                                                                                            'groupid' => 0]);
+                        // Create an event to assign the license.
+                        $eventother = array('licenseid' => $companylicenseid,
+                                            'issuedate' => $runtime,
+                                            'duedate' => $runtime);
+                        $event = \block_iomad_company_admin\event\user_license_assigned::create(array('context' => \context_course::instance($pathcourse),
+                                                                                                    'objectid' => $licenseuserid,
+                                                                                                    'courseid' => $pathcourse,
+                                                                                                    'userid' => $invoice->userid,
+                                                                                                    'other' => $eventother));
+                        $event->trigger();
+                    }
+                }
+                if (!empty($pathcourseenrol)) {
+                    foreach ($pathcourseenrol as $pathcourse) {
+                        if (!$DB->get_record('iomad_courses', ['courseid' => $pathcourse, 'licensed' => 1])) {
+                            // Enrol user into course.
+                            company_user::enrol($invoice->userid, [$pathcourse]);
+                        }
+                    }
                 }
                 $companypaths = new \local_iomad_learningpath\companypaths($companyid, context_system::instance());
                 // Add user to path(s)
@@ -283,10 +296,8 @@ class processor {
                 foreach ($paths as $path) {
                     $pathcourses = $DB->get_records('iomad_learningpathcourse',['path' => $path->pathid]);
                     foreach ($pathcourses as $pathcourse) {
-                        if ($DB->get_record('iomad_courses', ['courseid' => $pathcourse->course, 'licensed' => 1])) {
-                            $pathcoursesarray[] = $pathcourse->course;
-                            $totalcourses++;
-                        }
+                        $pathcoursesarray[] = $pathcourse->course;
+                        $totalcourses++;
                     }
                 }
                 $companylicense->allocation = $totalcourses;
@@ -303,7 +314,7 @@ class processor {
                                                                                         'issuedate' => $runtime,
                                                                                         'groupid' => 0]);
                 }
-            }else if (!empty($courses)) {
+            } else if (!empty($courses)) {
                 $companylicense->program = $item->program;
                 $companylicense->allocation = (empty($companylicense->program)) ? $invoiceitem->license_allocation : $invoiceitem->license_allocation * count($courses);
                 // Deal with license valid length.
