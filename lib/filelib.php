177cb1d2fa9c1fb85fd540fb67b46a52a7cbcf27
Robert <robert@e-learndesign.co.uk>
2025-08-06 17:11:06 +0100

    IOMAD: Fix URL for a file in a module desciption so the file loads for a user who is in multiple tenants - closes #2351 - closes #2350

--- lib/filelib.php
+++ lib/filelib.php
@@ -496,15 +496,15 @@ function file_rewrite_pluginfile_urls($text, $file, $contextid, $component, $fil
     if (!isset($options['forcehttps'])) {
         $options['forcehttps'] = false;
     }
-
-    $baseurl = "{$CFG->wwwroot}/{$file}";
+    
+    $baseurl = company::get_relativeurl("{$CFG->wwwroot}/{$file}");
     if (!empty($options['includetoken'])) {
         $userid = $options['includetoken'] === true ? $USER->id : $options['includetoken'];
         $token = get_user_key('core_files', $userid);
         $finalfile = basename($file);
         $tokenfile = "token{$finalfile}";
         $file = substr($file, 0, strlen($file) - strlen($finalfile)) . $tokenfile;
-        $baseurl = "{$CFG->wwwroot}/{$file}";
+        $baseurl = company::get_relativeurl("{$CFG->wwwroot}/{$file}");
 
         if (!$CFG->slasharguments) {
             $baseurl .= "?token={$token}&file=";
