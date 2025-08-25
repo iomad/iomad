045ed12324985a7595b06edbfc1864e8b876df82
Derick Turner <derick@e-learndesign.co.uk>
2025-08-25 22:15:10 +0100

    IOMAD: need to set version minor numbers so we can have separate plugins in the plugin DB - 4.0.1 - missed mod_trainingevent

--- mod/trainingevent/version.php
+++ mod/trainingevent/version.php
@@ -28,8 +28,8 @@ defined('MOODLE_INTERNAL') || die;
 
 $plugin->release  = '4.1.20 (Build: 20250811)'; // Human-friendly version name
 $plugin->component  = 'mod_trainingevent';
-$plugin->version  = 2025012301;  // The current module version (Date: YYYYMMDDXX).
-$plugin->requires = 2019052000.401;  // Requires this Moodle version.
+$plugin->version  = 2025012301.401;  // The current module version (Date: YYYYMMDDXX).
+$plugin->requires = 2019052000;  // Requires this Moodle version.
 $plugin->cron     = 0;           // Period for cron to check this module (secs).
 $plugin->dependencies = ['local_iomad' => 2023042702];
 $plugin->supported = [401, 401];
