c9695e48f2663db59ed755eb9db8ec71d3dcb593
Derick Turner <derick@e-learndesign.co.uk>
2025-09-12 00:12:00 +0100

    IOMAD: updated local_template_selector to pass local codechecker plugin PHP CS checks - #2474

--- local/template_selector/classes/privacy/provider.php
+++ local/template_selector/classes/privacy/provider.php
@@ -25,8 +25,6 @@
 
 namespace local_template_selector\privacy;
 
-defined('MOODLE_INTERNAL') || die();
-
 /**
  * Privacy Subsystem for local_template_selector implementing null_provider.
  *
@@ -41,7 +39,7 @@ class provider implements \core_privacy\local\metadata\null_provider {
      *
      * @return  string
      */
-    public static function get_reason() : string {
+    public static function get_reason(): string {
         return 'privacy:metadata';
     }
 }
