cf15a6034e48badb8279f464e0a3f64735d5e152
Derick Turner <derick@e-learndesign.co.uk>
2025-11-06 13:38:15 +0000

    IOMAD: Trainingevent signups should be cleared on user enrolment deleted - closes #2520 (missed version.php)

--- public/mod/trainingevent/version.php
+++ public/mod/trainingevent/version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->release  = '5.0.2 (Build: 20250811)';    // Human-friendly version name.
-$plugin->version  = 2025041450;  // The current module version (Date: YYYYMMDDXX).
+$plugin->release  = '5.0.3 (Build: 20251006)';    // Human-friendly version name.
+$plugin->version  = 2025110650;  // The current module version (Date: YYYYMMDDXX).
 $plugin->requires = 2025041400;   // Requires this Moodle version.
 $plugin->component  = 'mod_trainingevent';
 $plugin->dependencies = ['local_iomad' => 2025041400];
