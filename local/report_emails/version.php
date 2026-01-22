e874a3026253004dd9c1f0bed6242cd564ed075d
Derick Turner <derick@e-learndesign.co.uk>
2026-01-22 22:31:58 +0000

    IOMAD: re-write of local_report_emails to bring code up to current standards - #2468

--- local/report_emails/version.php
+++ local/report_emails/version.php
@@ -15,13 +15,17 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * IOMAD report emails
+ *
  * @package   local_report_emails
  * @copyright 2021 Derick Turner
  * @author    Derick Turner
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$plugin->release  = '4.4.12 (Build: 20251208)'; // Human-friendly version name
+defined('MOODLE_INTERNAL') || die();
+
+$plugin->release  = '4.4.12 (Build: 20251208)'; // Human-friendly version name.
 $plugin->version  = 2024042244;   // The (date) version of this plugin.
 $plugin->requires = 2024042200;   // Requires this Moodle version.
 $plugin->component  = 'local_report_emails';
