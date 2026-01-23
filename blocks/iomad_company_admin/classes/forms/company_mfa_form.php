48704b156327503670549ee408b33f9bc8db3d01
Derick Turner <derick@e-learndesign.co.uk>
2026-01-23 17:37:10 +0000

    IOMAD: start of the large task to re-write block_iomad_company_admin to bring code up to current standard - #2442 #2567

---
+++ blocks/iomad_company_admin/classes/forms/company_mfa_form.php
@@ -0,0 +1,104 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * @package   block_iomad_company_admin
+ * @copyright 2021 Derick Turner
+ * @author    Derick Turner
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace block_iomad_company_admin\forms;
+
+defined('MOODLE_INTERNAL') || die;
+
+use \moodle_url;
+use context_system;
+use tool_mfa;
+use moodleform;
+use \admin_setting;
+use local_iomad\company;
+use local_iomad\iomad;
+use local_iomad\company_user;
+
+require_once($CFG->libdir . '/adminlib.php');
+
+class company_mfa_form extends moodleform {
+    public function definition() {
+        global $CFG, $PAGE, $DB, $postfix;
+
+        $mform = & $this->_form;
+
+        $strrequired = get_string('required');
+
+        $mform->addElement('hidden', 'action');
+        $mform->setType('action', PARAM_ALPHA);
+
+        $mform->addElement('html', "<h2>" . format_string(get_string('mfasettings', 'tool_mfa') . " : " .
+                                                          get_string('settings', 'moodle')) . "</h2>");
+
+        // Get the tool table and details.
+        $managemfa = new tool_mfa\local\admin_setting_managemfa(true);
+        $mform->addElement('html', $managemfa->output_html([]));
+
+        $name = get_string('settings:enabled', 'tool_mfa');
+        $mform->addElement('advcheckbox', 'enabled' . $postfix, $name);
+        $mform->setDefault('enabled' . $postfix, false);
+
+        $name = get_string('settings:lockout', 'tool_mfa');
+        $description = get_string('settings:lockout_help', 'tool_mfa');
+        $mform->addElement('text', 'lockout' . $postfix, $name);
+        $mform->addElement('static', 'lockoutdesc', '', $description);
+        $mform->setDefault('lockout' . $postfix, 10);
+        $mform->setType('lockout' . $postfix, PARAM_INT);
+
+        $name = get_string('settings:debugmode', 'tool_mfa');
+        $description = get_string('settings:debugmode_help', 'tool_mfa');
+        $mform->addElement('advcheckbox', 'debugmode' . $postfix, $name);
+        $mform->addElement('static', 'debugmodedesc', '', $description);
+        $mform->setDefault('debugmode' . $postfix, false);
+
+        $name = get_string('settings:redir_exclusions', 'tool_mfa');
+        $description = get_string('settings:redir_exclusions_help', 'tool_mfa');
+        $mform->addElement('textarea', 'redir_exclusions' . $postfix, $name);
+        $mform->addElement('static', 'redir_exclusionsdesc', '', $description);
+        $mform->setType('redir_exclusions' . $postfix, PARAM_RAW);
+
+        $name = get_string('settings:guidancecheck', 'tool_mfa');
+        $description = get_string('settings:guidancecheck_help', 'tool_mfa');
+        $mform->addElement('advcheckbox', 'guidance' . $postfix, $name);
+        $mform->addElement('static', ',guidancedesc', '', $description);
+        $mform->setDefault('guidance' . $postfix, false);
+
+        $name = get_string('settings:guidancepage', 'tool_mfa');
+        $description = get_string('settings:guidancepage_help', 'tool_mfa');
+        $mform->addElement('textarea', 'guidancecontent' . $postfix, $name);
+        $mform->addElement('static', 'guidancecontentdesc', '', $description);
+        $mform->setType('guidancecontent' . $postfix, PARAM_RAW);
+
+        $name = get_string('settings:guidancefiles', 'tool_mfa');
+        $description = get_string('settings:guidancefiles_help', 'tool_mfa');
+        $mform->addElement('filemanager', 'guidancefiles' . $postfix, $name, 0, [
+            'maxfiles' => -1,
+                ]);
+        $mform->addElement('static', 'guidancefilesdesc', '', $description);
+
+        // Disable the onchange popup.
+        $mform->disable_form_change_checker();
+
+        $this->add_action_buttons();
+    }
+}
