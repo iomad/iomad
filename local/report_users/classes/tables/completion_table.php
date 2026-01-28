8327e9aaf78419d329aabb2b74a28b6484c956d7
Derick Turner <derick@e-learndesign.co.uk>
2026-01-28 10:50:12 +0000

    IOMAD: typo in local_report_users\tables\completion_table class meaning completion progress is not displayed - #2473

--- local/report_users/classes/tables/completion_table.php
+++ local/report_users/classes/tables/completion_table.php
@@ -542,7 +542,7 @@ class completion_table extends table_sql {
                                 'div',
                                 '0%',
                                 [
-                                    'class' > 'progress-bar',
+                                    'class' => 'progress-bar',
                                     'style' => 'width:0%;height:20px',
                                 ]
                             ) .
@@ -568,7 +568,7 @@ class completion_table extends table_sql {
                                 'div',
                                 '0%',
                                 [
-                                    'class' > 'progress-bar',
+                                    'class' => 'progress-bar',
                                     'style' => 'width:0%;height:20px',
                                 ]
                             ) .
@@ -603,7 +603,7 @@ class completion_table extends table_sql {
                                 'div',
                                 $progress . '%',
                                 [
-                                    'class' > 'progress-bar',
+                                    'class' => 'progress-bar',
                                     'style' => 'width:' . $progress . '%;height:20px',
                                 ]
                             ) .
