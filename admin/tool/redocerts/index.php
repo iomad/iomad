c1ed02c9e671b16af1355067c1ef194ce2751dcf
Derick Turner <derick@e-learndesign.co.uk>
2026-01-31 21:15:36 +0000

    IOMAD: redocerts should redirect to IOMAD dashboard.  Also ID number is no longer part of the form - #2486

--- admin/tool/redocerts/index.php
+++ admin/tool/redocerts/index.php
@@ -72,7 +72,6 @@ echo $OUTPUT->box_start();
 do_redocerts($data->user,
              $data->course,
              $data->company,
-             $data->idnumber,
              $data->fromdate,
              $data->todate,
              $data->userid,
@@ -82,6 +81,6 @@ echo $OUTPUT->box_end();
 
 // Course caches are now rebuilt on the fly.
 
-echo $OUTPUT->continue_button(new moodle_url('/admin/index.php'));
+echo $OUTPUT->continue_button(new moodle_url('/blocks/iomad_company_admin/index.php'));
 
 echo $OUTPUT->footer();
