690df5a9c9263c833686d32d2006bbecf6275594
Derick Turner <derick@e-learndesign.co.uk>
2026-02-03 15:00:17 +0000

    IOMAD: Not enough capabilities in learning paths - closes #2584

--- local/iomad_learningpath/version.php
+++ local/iomad_learningpath/version.php
@@ -18,14 +18,15 @@
  * Version file for Iomad Learning Paths
  *
  * @package    local_iomadlearninpath
- * @copyright  2018 Howard Miller (howardsmiller@gmail.com)
+ * @copyright  2018 e-Learn Design Ltd. https://www.e-learndesign.co.uk
+ * @author     Howard Miller (howardsmiller@gmail.com)
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->release  = '5.0.4 (Build: 20251208)';    // Human-friendly version name
-$plugin->version  = 2025041450;   // The (date) version of this plugin.
+$plugin->version  = 2025121150;   // The (date) version of this plugin.
 $plugin->requires = 2025041400;   // Requires this Moodle version.
 $plugin->component = 'local_iomad_learningpath';
 $plugin->dependencies = ['local_iomad' => 2025041400];
