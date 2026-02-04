dff06a6975d937bcbac9399c133150f5d561c30c
Derick Turner <derick@e-learndesign.co.uk>
2026-02-04 16:34:01 +0000

    IOMAD: #2441 - needed version bump for upgrade.php to get triggered

--- public/blocks/iomad_commerce/version.php
+++ public/blocks/iomad_commerce/version.php
@@ -25,8 +25,8 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release  = '5.0.2 (Build: 20250811)';    // Human-friendly version name
-$plugin->version  = 2026012050;   // The (date) version of this plugin.
+$plugin->release  = '5.0.2 (Build: 20250811)';    // Human-friendly version name.
+$plugin->version  = 2026013150;   // The (date) version of this plugin.
 $plugin->requires = 2025041400;   // Requires this Moodle version.
 $plugin->component = 'block_iomad_commerce';
 $plugin->dependencies = ['local_iomad' => 2025041400];
