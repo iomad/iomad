6fb6da88e204f73b8f8b399b2e1393683a4112d2
Derick Turner <derick@e-learndesign.co.uk>
2026-02-10 11:55:45 +0000

    IOMAD: company user class update for strictness and standardisation of function doc blocks. Issues with default company_user in iomad requiring a parameter so updated company_user::is_company_user to pass derick as default and updated code which called iomad::is_company_user() without passing a user object to call the company_user one instead - #2456

--- lib/navigationlib.php
+++ lib/navigationlib.php
@@ -1812,7 +1812,7 @@ class global_navigation extends navigation_node {
             if (!isloggedin()) {
                 return array();
             }
-            if (iomad::is_company_user()) {
+            if (company_user::is_company_user()) {
                 $companyid = iomad::get_my_companyid(context_system::instance());
                 $sharedsql = " AND ( c.id IN (
                                    SELECT courseid FROM {company_course}
@@ -1954,7 +1954,7 @@ class global_navigation extends navigation_node {
         } else {
             // Prepare the SQL to load the courses and their contexts
             // IOMAD addition.
-            if (iomad::is_company_user()) {
+            if (company_user::is_company_user()) {
                 $companyid = iomad::get_my_companyid(context_system::instance());
                 $sharedsql = " AND ( c.id IN (
                                    SELECT courseid FROM {company_course}
@@ -3761,7 +3761,7 @@ class global_navigation_for_ajax extends global_navigation {
             $limit = (int)$CFG->navcourselimit;
         }
 
-        if (iomad::is_company_user()) {
+        if (company_user::is_company_user()) {
             $companyid = iomad::get_my_companyid(context_system::instance());
             $sharedsql = " AND ( cc.id IN (
                                SELECT category FROM {company}
