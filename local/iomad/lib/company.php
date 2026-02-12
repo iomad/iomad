069a0f0c8bb1ecc818c02bf9edb8df4251cccbdb
Derick Turner <derick@e-learndesign.co.uk>
2026-02-12 18:24:12 +0000

    IOMAD: more dashboard pages brought into line with code checker - #2442

--- local/iomad/lib/company.php
+++ local/iomad/lib/company.php
@@ -3771,6 +3771,8 @@ class company {
                 $DB->delete_records('company_course_groups', ['groupid' => $groupid]);
             }
         }
+        
+        return true;
     }
 
     /**
