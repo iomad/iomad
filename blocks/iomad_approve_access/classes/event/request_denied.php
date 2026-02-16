a55527ab13a41da976e6b3b12e2892d6f6fa7a7f
Derick Turner <derick@e-learndesign.co.uk>
2026-02-16 01:11:30 +0000

    IOMAD: event URLs wrong - blocks not block - #2436

--- blocks/iomad_approve_access/classes/event/request_denied.php
+++ blocks/iomad_approve_access/classes/event/request_denied.php
@@ -75,7 +75,7 @@ class request_denied extends base {
      * @return \moodle_url
      */
     public function get_url() {
-        return new moodle_url('/block/iomad_approve_access/approve.php');
+        return new moodle_url('/blocks/iomad_approve_access/approve.php');
     }
 
     /**
