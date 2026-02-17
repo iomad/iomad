3ed457360089108a0929142bd8e187e7f99c7922
Derick Turner <derick@e-learndesign.co.uk>
2026-02-17 22:59:04 +0000

    IOMAD: update local_report_completion to meet new code standards - #2465

--- local/report_completion/db/access.php
+++ local/report_completion/db/access.php
@@ -15,24 +15,28 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
+ * IOMAD course completion report capabilities
+ *
  * @package   local_report_completion
  * @copyright 2021 Derick Turner
  * @author    Derick Turner
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$capabilities = array(
+defined('MOODLE_INTERNAL') || die;
 
-    'local/report_completion:view' => array(
+$capabilities = [
+
+    'local/report_completion:view' => [
         'riskbitmask' => RISK_PERSONAL,
         'captype' => 'read',
         'contextlevel' => CONTEXT_COMPANY,
-        'archetypes' => array(
+        'archetypes' => [
             'companymanager' => CAP_ALLOW,
             'companydepartmentmanager' => CAP_ALLOW,
             'clientadministrator' => CAP_ALLOW,
             'clientreporter' => CAP_ALLOW,
-            'companyreporter' => CAP_ALLOW
-        ),
-    )
-);
+            'companyreporter' => CAP_ALLOW,
+        ],
+    ],
+];
