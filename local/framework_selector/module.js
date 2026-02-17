99b5d8daeccd9b2ec931f627d9ff3cbb304ddba6
Derick Turner <derick@e-learndesign.co.uk>
2026-02-17 12:18:39 +0000

    IOMAD: 4.5/5.0 only updates to selectors to fix display issues with buttons and the form elements in general.  Added missing default values to base user selector class to prevent deprecated warnings - #2452 #2455 #2457

--- local/framework_selector/module.js
+++ local/framework_selector/module.js
@@ -75,7 +75,7 @@ M.local_framework_selector.init_framework_selector = function (Y, name, hash, ex
 
             // Replace the Clear submit button with a clone that is not a submit button.
             var clearbtn = Y.one('#' + this.name + '_clearbutton');
-            this.clearbutton = Y.Node.create('<input type="button" value="' + clearbtn.get('value') + '" />');
+            this.clearbutton = Y.Node.create('<input type="button" value="' + clearbtn.get('value') + '" class="btn btn-secondary mx-1"/>');
             clearbtn.replace(Y.Node.getDOMNode(this.clearbutton));
             this.clearbutton.set('id', + this.name + "_clearbutton");
             this.clearbutton.on('click', this.handle_clear, this);
