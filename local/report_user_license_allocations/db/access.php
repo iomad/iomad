e474c41b9644a81ebe2a26b22b030b07940a21af
Derick Turner <derick@e-learndesign.co.uk>
2026-02-18 19:22:35 +0000

    IOMAD: updated local_report_license_allocations to meet new code standards - closes #2470

--- local/report_user_license_allocations/db/access.php
+++ local/report_user_license_allocations/db/access.php
@@ -15,24 +15,28 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package   local_report_license_usage
+ * IOMAD user license allocations report capabilities
+ *
+ * @package   local_report_user_license_allocations
  * @copyright 2021 Derick Turner
  * @author    Derick Turner
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$capabilities = array(
+defined('MOODLE_INTERNAL') || die;
 
-    'local/report_user_license_allocations:view' => array(
+$capabilities = [
+
+    'local/report_user_license_allocations:view' => [
         'riskbitmask' => RISK_PERSONAL,
         'captype' => 'read',
         'contextlevel' => CONTEXT_COMPANY,
-        'archetypes' => array(
+        'archetypes' => [
             'companymanager' => CAP_ALLOW,
             'companydepartmentmanager' => CAP_ALLOW,
             'clientadministrator' => CAP_ALLOW,
             'clientreporter' => CAP_ALLOW,
-            'companyreporter' => CAP_ALLOW
-        ),
-    )
-);
+            'companyreporter' => CAP_ALLOW,
+        ],
+    ],
+];
