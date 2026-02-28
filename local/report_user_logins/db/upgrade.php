3c726f647b69c6492aff9435a5bd82e01be455bf
Derick Turner <derick@e-learndesign.co.uk>
2026-02-28 17:50:56 +0000

    IOMAD: update local_report_user_logins table to reflect table, index and key name standards - #2472 #2567

--- local/report_user_logins/db/upgrade.php
+++ local/report_user_logins/db/upgrade.php
@@ -37,64 +37,24 @@ function xmldb_local_report_user_logins_upgrade($oldversion) {
     $result = true;
     $dbman = $DB->get_manager();
 
-    if ($oldversion < 2019012100) {
+    if ($oldversion < 2026022800) {
 
-        upgrade_set_timeout(7200); // Set installation time to 2 hours as this takes a long time.
-
-        // Define table local_report_user_logins to be created.
+        // Define key userid (foreign-unique) to be dropped form local_report_user_logins.
         $table = new xmldb_table('local_report_user_logins');
+        $key = new xmldb_key('userid', XMLDB_KEY_FOREIGN_UNIQUE, ['userid'], 'user', ['id']);
 
-        // Adding fields to table local_report_user_logins.
-        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
-        $table->add_field('userid', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('created', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
-        $table->add_field('firstlogin', XMLDB_TYPE_INTEGER, '20', null, null, null, null);
-        $table->add_field('lastlogin', XMLDB_TYPE_INTEGER, '20', null, null, null, null);
-        $table->add_field('logincount', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, '0');
-        $table->add_field('modifiedtime', XMLDB_TYPE_INTEGER, '20', null, XMLDB_NOTNULL, null, null);
+        // Launch drop key userid.
+        $dbman->drop_key($table, $key);
 
-        // Adding keys to table local_report_user_logins.
-        $table->add_key('primary', XMLDB_KEY_PRIMARY, ['id']);
-        $table->add_key('userid', XMLDB_KEY_UNIQUE, ['userid']);
+        // Define key fk_userid (foreign-unique) to be added to local_report_user_logins.
+        $table = new xmldb_table('local_report_user_logins');
+        $key = new xmldb_key('fk_userid', XMLDB_KEY_FOREIGN_UNIQUE, ['userid'], 'user', ['id']);
 
-        // Conditionally launch create table for local_report_user_logins.
-        if (!$dbman->table_exists($table)) {
-            $dbman->create_table($table);
-        }
-
-        $DB->execute("INSERT INTO {local_report_user_logins} (userid, created, firstlogin, lastlogin, logincount, modifiedtime)
-                      SELECT id AS userid,
-                      timecreated,
-                      NULLIF(firstaccess,0) AS firstaccess,
-                      NULLIF(currentlogin,0) AS currentlogin,
-                      CASE
-                          WHEN currentlogin = 0
-                          THEN 0
-                          ELSE (
-                              SELECT COUNT(id)
-                              FROM {logstore_standard_log} l
-                              WHERE u.id = l.userid
-                              AND eventname = :eventname)
-                      END AS totallogins,
-                      :modifiedtime as modifiedtime
-                      FROM {user} u",
-                     ['eventname' => '\core\event\user_loggedin',
-                      'modifiedtime' => time()]);
-
-        // Deal with any that may have been missed.
-        if ($missedusers = $DB->get_records_sql("SELECT u.* FROM {user} u
-                                                 JOIN {local_report_user_logins} lrul ON (u.id = lrul.userid)
-                                                 WHERE lrul.logincount = 0
-                                                 AND u.currentlogin != 0")) {
-            foreach ($missedusers as $missed) {
-                // Not in the logs to we are going to set it to 1 as it's the only evidence we have.
-                $DB->set_field('local_report_user_logins', 'logincount', 1, ['userid' => $missed->id]);
-                $DB->set_field('local_report_user_logins', 'lastlogin', $missed->currentlogin, ['userid' => $missed->id]);
-            }
-        }
+        // Launch add key fk_userid.
+        $dbman->add_key($table, $key);
 
         // Report_user_logins savepoint reached.
-        upgrade_plugin_savepoint(true, 2019012100, 'local', 'report_user_logins');
+        upgrade_plugin_savepoint(true, 2026022800, 'local', 'report_user_logins');
     }
 
     return $result;
