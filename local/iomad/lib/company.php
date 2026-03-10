d9044395233024f1bc23f7d334a18aee4d107ee7
Derick Turner <derick@e-learndesign.co.uk>
2026-03-10 16:50:25 +0000

    IOMAD: Company managers gets mixed "Course Access" and "Course Editor" roles - patch by @tom5850 - closes #2334

--- local/iomad/lib/company.php
+++ local/iomad/lib/company.php
@@ -897,7 +897,7 @@ class company {
                         if ($user = $DB->get_record('user', ['id' => $educator->userid,
                                                              'deleted' => 0]) ) {
                             if ($DB->record_exists('course', ['id' => $course->id])) {
-                                if ($DB->record_exists('iomad_courses', ['courseid' => $course->id, 'shared' => 1])) {
+                                if (!$own) {
                                     // Not created by a company manager.
                                     company_user::enrol($user,
                                                         [$course->id],
