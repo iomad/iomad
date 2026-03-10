7aa0b1b98e34b122e89c1b0113a8e7efc65331db
Derick Turner <derick@e-learndesign.co.uk>
2026-03-10 23:09:40 +0000

    IOMAD: 4.5E additions for #2619

--- local/iomad/db/install.php
+++ local/iomad/db/install.php
@@ -23,6 +23,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use local_iomad\task\langpackinitialinstall;
 use local_iomad\task\resetrolestask;
 use core\task\manager;
 
