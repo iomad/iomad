96d64c456a0b3c7f5aa4a800fa24ae02070cea97
Derick Turner <derick@e-learndesign.co.uk>
2026-03-20 15:16:56 +0000

    IOMAD: theme setting_file_url function is no longer in theme core_renderer - #2543

--- public/theme/iomadboost/classes/output/core_renderer.php
+++ public/theme/iomadboost/classes/output/core_renderer.php
@@ -324,37 +324,4 @@ class core_renderer extends \core_renderer {
         ));
         return $button;
     }
-
-    /**
-     * Returns URL to the stored file via pluginfile.php.
-     *
-     * Note the theme must also implement pluginfile.php handler,
-     * theme revision is used instead of the itemid.
-     *
-     * @param string $setting
-     * @param string $filearea
-     * @return string protocol relative URL or null if not present
-     */
-    public function setting_file_url($setting, $filearea) {
-        global $CFG;
-
-            return null;
-        if (empty($this->settings->$setting)) {
-            return null;
-        }
-
-        $component = 'theme_'.$this->name;
-        $itemid = theme_get_revision();
-        $filepath = $this->settings->$setting;
-        $syscontext = context_system::instance();
-
-        $url = moodle_url::make_file_url("$CFG->wwwroot/pluginfile.php", "/$syscontext->id/$component/$filearea/$itemid".$filepath);
-
-        // Now this is tricky because the we can not hardcode http or https here, lets use the relative link.
-        // Note: unfortunately moodle_url does not support //urls yet.
-
-        $url = preg_replace('|^https?://|i', '//', $url->out(false));
-
-        return $url;
-    }
 }
