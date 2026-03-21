82e16d8813dc51becbffa48bc54739929edeed0d
Derick Turner <derick@e-learndesign.co.uk>
2026-03-21 16:40:30 +0000

    IOMAD: admin links area incorrect in iomadsaml2 auth plugin - closes #2565

--- auth/iomadsaml2/certificatelock.php
+++ auth/iomadsaml2/certificatelock.php
@@ -47,7 +47,7 @@ if ($companyid > 0) {
 
 $form = new \auth_iomadsaml2\form\lockcertificate();
 
-$settingspage = new moodle_url('/admin/settings.php?section=authsettingsaml2');
+$settingspage = new moodle_url('/admin/settings.php?section=authsettingiomadsaml2');
 
 if ($data = $form->get_data()) {
 
