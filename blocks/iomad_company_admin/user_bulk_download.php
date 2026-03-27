iomad/iomad

commit af268b17cb8e24b92ef62faf59ca63c06b02b17f

IOMAD: issues introduced with recent changes to edit users not filtering by department properly, issues with dashboard on index page not being rendered properly - #2442

blocks/iomad_company_admin/user_bulk_download.php
@@ -129,15 +129,11 @@ if ($format) {
         $departmentusers = company::get_recursive_department_users($userlevelid);
     }
     if (count($departmentusers) > 0) {
-        $departmentids = "";
-        foreach ($departmentusers as $departmentuser) {
-            if (!empty($departmentids)) {
-                $departmentids .= ",".$departmentuser->userid;
-            } else {
-                $departmentids .= $departmentuser->userid;
-            }
-        }
-        $sqlsearch = " AND userid in ($departmentids) ";
+        [$insql, $inparams] = $DB->get_in_or_equal(array_keys($departmentusers),
+                                                   SQL_PARAMS_NAMED,
+                                                   'duids');
+        $sqlsearch = " AND u.id {$insql} ";
+        $params = $params + $inparams;
     } else {
         $sqlsearch = "AND 1 = 0";
     }

blocks/iomad_company_admin/editusers.php
@@ -53,9 +53,9 @@ $perpage = optional_param('perpage', $CFG->iomad_max_list_users, PARAM_INT);    
 $acl = optional_param('acl', '0', PARAM_INT);           // Id of user to tweak mnet ACL (requires $access).
 $search = optional_param('search', '', PARAM_CLEAN);// Search string.
 $departmentid = optional_param('deptid', 0, PARAM_INTEGER);
-$firstname = optional_param('firstname', 0, PARAM_CLEAN);
+$firstname = optional_param('firstname', '', PARAM_CLEAN);
 $lastname = optional_param('lastname', '', PARAM_CLEAN);   // Md5 confirmation hash.
-$email = optional_param('email', 0, PARAM_CLEAN);
+$email = optional_param('email', '', PARAM_CLEAN);
 $showall = optional_param('showall', false, PARAM_BOOL);
 $usertype = optional_param('usertype', 'a', PARAM_ALPHANUM);
 $edit = optional_param('edit', -1, PARAM_BOOL);
@@ -89,7 +89,7 @@ $company = new company($companyid);
 iomad::require_capability('block/iomad_company_admin:view_editusers', $companycontext);
 
 // Are we able to view all users regardless of tenant?
-if (!iomad::has_capability('block/iomad_company_admin:company_add', $companycontext)) {
+if (!iomad::has_capability('block/iomad_company_admin:company_add', $systemcontext)) {
     $showall = false;
 }
 $params['showall'] = $showall;
@@ -492,41 +492,28 @@ if (!empty($CFG->iomad_report_fields)) {
 $searchinfo = iomad::get_user_sqlsearch($params, $idlist, $sort, $dir, $departmentid, true, true);
 
 // Set some defaults.
-$sqlsearch = " AND 1 = 2 ";
+if ($showall) {
+    $departmentsearch = "";
+} else {
+    $departmentsearch = " AND 1 = 2 ";
+}
+$sqlsearch = " AND u.id NOT IN (" . $CFG->siteadmins . ")";
 $sqlparams = [];
 $managertypesql = "";
 $companysql = "";
 
-// Get all or company users depending on capability.
-if (iomad::has_capability('block/iomad_company_admin:editallusers', $companycontext)) {
-    // Make sure we dont display site admins.
-    // Set default search to something which cant happen.
-    $sqlsearch = " AND u.id NOT IN (" . $CFG->siteadmins . ")";
-
-    // Get department users.
-    $departmentusers = company::get_recursive_department_users($departmentid);
-    if (count($departmentusers) > 0 || $showall) {
-        if (!$showall) {
-            [$insql, $inparams] = $DB->get_in_or_equal(array_keys($departmentusers),
-                                                       SQL_PARAMS_NAMED,
-                                                       'duids');
-            $sqlsearch .= " AND u.id {$insql} ";
-            $sqlparams = $sqlparams + $inparams;
-        }
-    }
-} else {
-    // Get users company association.
-    $departmentusers = company::get_recursive_department_users($departmentid);
-    if (count($departmentusers) > 0) {
-        if (empty($showsuspended)) {
-            [$insql, $inparams] = $DB->get_in_or_equal(array_keys($departmentusers),
-                                                       SQL_PARAMS_NAMED,
-                                                       'duids');
-            $sqlsearch = " AND u.id {$insql} ";
-            $sqlparams = $sqlparams + $inparams;
-        }
+// Get department users.
+$departmentusers = company::get_recursive_department_users($departmentid);
+if (count($departmentusers) > 0) {
+    if (!$showall) {
+        [$insql, $inparams] = $DB->get_in_or_equal(array_keys($departmentusers),
+                                                   SQL_PARAMS_NAMED,
+                                                   'duids');
+        $departmentsearch = " AND u.id {$insql} ";
+        $sqlparams = $sqlparams + $inparams;
     }
 }
+$sqlsearch .= $departmentsearch;
 
 // Return the right type of user.
 if ($usertype != 'a' ) {

blocks/iomad_company_admin/index.php
@@ -44,9 +44,6 @@ require_login();
 // Deal with initial company stuff.
 $systemcontext = context_system::instance();
 $companycontext = $systemcontext;
-if (!empty($company)) {
-    $companycontext = \core\context\company::instance($company);
-}
 
 // Did we change company?
 if ($companychange &&
@@ -145,6 +142,11 @@ if (empty($SESSION->currenteditingcompany) &&
     } else {
         $company = 0;
     }
+}
+
+// _Now_ we set the context.
+if (!empty($company)) {
+    $companycontext = \core\context\company::instance($company);
 }
 
 // Page setup stuff.

commit ab617e0943f390a2ca64ead57fa42bb76ce15e54

IOMAD: commit af268b17cb8e24b92ef62faf59ca63c06b02b17f caused issues with user bulk download - should have been u.userid not u.id - #2442

blocks/iomad_company_admin/user_bulk_download.php
@@ -23,6 +23,8 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
+use core\user;
+
 require_once(__DIR__ . '/../../config.php');
 require_once($CFG->libdir.'/adminlib.php');
 require_once(__DIR__ . '/lib.php');
@@ -94,9 +96,9 @@ if ($format) {
     // Get company category.
     if ($category = $DB->get_record_sql(
         "SELECT uic.id, uic.name
-         FROM {user_info_category} uic
-         JOIN {company} c ON (uic.id = c.profileid)
-         WHERE c.id = :companyid",
+           FROM {user_info_category} uic
+           JOIN {company} c ON (uic.id = c.profileid)
+          WHERE c.id = :companyid",
         ['companyid' => $companyid])) {
         if ($extrafields = $DB->get_records('user_info_field', ['categoryid' => $category->id])) {
             foreach ($extrafields as $n => $v) {
@@ -107,10 +109,10 @@ if ($format) {
     // Get non company categories.
     if ($categories = $DB->get_records_sql(
         "SELECT id, name
-         FROM {user_info_category}
-         WHERE id NOT IN (
-             SELECT profileid FROM {company}
-         )")) {
+           FROM {user_info_category}
+          WHERE id NOT IN (
+              SELECT profileid FROM {company}
+          )")) {
         foreach ($categories as $category) {
             if ($extrafields = $DB->get_records('user_info_field', ['categoryid' => $category->id])) {
                 foreach ($extrafields as $n => $v) {
@@ -126,26 +128,24 @@ if ($format) {
     $departmentusers = [];
     $userlevels = $company->get_userlevel($USER);
     foreach ($userlevels as $userlevelid => $userlevel) {
-        $departmentusers = company::get_recursive_department_users($userlevelid);
+        $departmentusers = $departmentusers + company::get_recursive_department_users($userlevelid);
     }
     if (count($departmentusers) > 0) {
         [$insql, $inparams] = $DB->get_in_or_equal(array_keys($departmentusers),
                                                    SQL_PARAMS_NAMED,
                                                    'duids');
-        $sqlsearch = " AND u.id {$insql} ";
+        $sqlsearch = " AND userid {$insql} ";
         $params = $params + $inparams;
     } else {
         $sqlsearch = "AND 1 = 0";
     }
 
-
-
-    $userids = $DB->get_records_sql_menu("SELECT DISTINCT userid, userid as id
-        FROM
-            {company_users} u
-        WHERE
-            companyid = :companyid
-            " . $sqlsearch, $params);
+    $userids = $DB->get_records_sql(
+        "SELECT DISTINCT userid AS id
+                    FROM {company_users}
+                   WHERE companyid = :companyid
+                         $sqlsearch",
+        $params);
 
     switch ($format) {
         case 'csv' : user_download_csv($userids, $fields, ! $companyid);
@@ -182,7 +182,7 @@ echo $OUTPUT->footer();
  * @return void
  */
 function user_download_ods($userids, $fields, $includecompanyfield) {
-    global $CFG, $SESSION, $DB;
+    global $CFG;
 
     require_once("$CFG->libdir/odslib.class.php");
     require_once($CFG->dirroot.'/user/profile/lib.php');
@@ -205,15 +205,15 @@ function user_download_ods($userids, $fields, $includecompanyfield) {
     $worksheet[0]->write(0, $col, 'temppassword');
 
     $row = 1;
-    foreach ($userids as $userid) {
+    foreach (array_keys($userids) as $userid) {
         // Stop the script from timing out on large numbers of users.
         set_time_limit(30);
-        if (!$user = $DB->get_record('user', ['id' => $userid])) {
+        if (!$user = user::get_user($userid)) {
             continue;
         }
         $col = 0;
         profile_load_data($user);
-        foreach ($fields as $field => $unused) {
+        foreach (array_keys($fields) as $field) {
             // Stop the script from timing out on large numbers of users.
             set_time_limit(30);
             if ($includecompanyfield || $field != "profile_field_company") {
@@ -244,7 +244,7 @@ function user_download_ods($userids, $fields, $includecompanyfield) {
  * @return void
  */
 function user_download_xls($userids, $fields, $includecompanyfield) {
-    global $CFG, $SESSION, $DB;
+    global $CFG;
 
     require_once("$CFG->libdir/excellib.class.php");
     require_once($CFG->dirroot.'/user/profile/lib.php');
@@ -267,15 +267,15 @@ function user_download_xls($userids, $fields, $includecompanyfield) {
     $worksheet[0]->write(0, $col, 'temppassword');
 
     $row = 1;
-    foreach ($userids as $userid) {
+    foreach (array_keys($userids) as $userid) {
         // Stop the script from timing out on large numbers of users.
         set_time_limit(30);
-        if (!$user = $DB->get_record('user', ['id' => $userid])) {
+        if (!$user = user::get_user($userid)) {
             continue;
         }
         $col = 0;
         profile_load_data($user);
-        foreach ($fields as $field => $unused) {
+        foreach (array_keys($fields) as $field) {
             // Stop the script from timing out on large numbers of users.
             set_time_limit(30);
             if ($includecompanyfield || $field != "profile_field_company") {
@@ -301,7 +301,7 @@ function user_download_xls($userids, $fields, $includecompanyfield) {
  * CSV Download processor
  */
 function user_download_csv($userids, $fields, $includecompanyfield) {
-    global $CFG, $SESSION, $DB;
+    global $CFG;
 
     require_once($CFG->dirroot.'/user/profile/lib.php');
 
@@ -325,15 +325,15 @@ function user_download_csv($userids, $fields, $includecompanyfield) {
     $row[] = "temppassword";
     echo implode($delimiter, $row)."\n";
 
-    foreach ($userids as $userid) {
+    foreach (array_keys($userids) as $userid) {
         // Stop the script from timing out on large numbers of users.
         set_time_limit(30);
         $row = [];
-        if (!$user = $DB->get_record('user', ['id' => $userid])) {
+        if (!$user = user::get_user($userid)) {
             continue;
         }
         profile_load_data($user);
-        foreach ($fields as $field => $unused) {
+        foreach (array_keys($fields) as $field) {
             // Stop the script from timing out on large numbers of users.
             set_time_limit(30);
             if ($includecompanyfield || $field != "profile_field_company") {