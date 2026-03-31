eb0b72a7f93a5d7f82389363d876903b37a85253
Derick Turner <derick@e-learndesign.co.uk>
2026-03-31 17:44:24 +0100

    IOMAD: OIDC sync with group passed won't work with expand=manager option - #2583

--- local/iomad_oidc_sync/classes/oidc_sync.php
+++ local/iomad_oidc_sync/classes/oidc_sync.php
@@ -436,7 +436,7 @@ class oidc_sync {
         } else {
             $graphurl = 'https://graph.microsoft.com/v1.0/groups/' .
                         $syncgroupid .
-                        '/members?$expand=manager&' .
+                        '/members?' .
                         $select .
                         '$top=500';
         }
