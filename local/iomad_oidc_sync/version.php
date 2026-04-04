7f9609468360e1e8e0e4804a5563f7dda39d6430
Derick Turner <derick@e-learndesign.co.uk>
2026-04-04 13:50:56 +0100

    IOMAD: local_iomad_oidc_sync not picked up in version release updated - 4.5E only

--- local/iomad_oidc_sync/version.php
+++ local/iomad_oidc_sync/version.php
@@ -26,7 +26,10 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->release  = '4.5.6 (Build: 20250811)'; // Human-friendly version name.
+$plugin->release  = '4.5.8 (Build: 20251208)'; // Human-friendly version name.
 $plugin->version  = 2024122345;   // The (date) version of this plugin.
-$plugin->requires = 2019052000;   // Requires this Moodle version.
+$plugin->requires = 2024100700;   // Requires this Moodle version.
 $plugin->component  = 'local_iomad_oidc_sync';
+$plugin->dependencies = ['local_iomad' => 2024090401];
+$plugin->supported = [405, 405];
+$plugin->maturity = MATURITY_STABLE;
