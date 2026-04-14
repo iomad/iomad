4ba65d3bec7de4300aea79861716d018a9ce443a
Derick Turner <derick@e-learndesign.co.uk>
2026-04-14 16:40:51 +0100

    IOMAD: Completion overview report missing entries if the user is on multiple companies - closes #2646

--- local/report_completion_overview/index.php
+++ local/report_completion_overview/index.php
@@ -611,7 +611,8 @@ $coursedetailsql = "SELECT lit.*
                       SELECT MAX(id)
                       FROM {local_iomad_track}
                       WHERE userid = lit.userid
-                      AND courseid = lit.courseid)";
+                      AND courseid = lit.courseid
+                      AND companyid = lit.companyid)";
 
 // Showing by user.
 if (!$bycourse) {
