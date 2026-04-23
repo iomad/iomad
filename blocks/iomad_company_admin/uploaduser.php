c04736c85f41ed7cdd483dd999934803baf49a8a
Derick Turner <derick@e-learndesign.co.uk>
2026-04-23 16:58:00 +0100

    IOMAD: needed to add the course context for the form check too - #2664

--- blocks/iomad_company_admin/uploaduser.php
+++ blocks/iomad_company_admin/uploaduser.php
@@ -1281,6 +1281,7 @@ if (!empty($cancelled)) {
                 }
                 foreach ($courseids as $courseid) {
                     // Are they already enrolled within this tenant?
+                    $coursecontext = context_course::instance($courseid);
                     if (!is_enrolled($coursecontext, $user->id) ||
                         !$DB->record_exists(
                             'local_iomad_track',
