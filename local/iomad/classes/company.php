e6ec0c9d2609f940cf6dce8f5754dd4c9e269081
Derick Turner <derick@e-learndesign.co.uk>
2026-04-30 15:00:07 +0100

    IOMAD: Error when changing theme in Edit Company when using PostgreSQL - closes #2669

--- local/iomad/classes/company.php
+++ local/iomad/classes/company.php
@@ -1342,7 +1342,7 @@ class company {
             'companyidforjoin' => $this->id,
         ];
 
-        $sql = "SELECT DISTINCT u.id, u.id AS mid
+        $sql = "SELECT DISTINCT u.id, u.id AS mid, u.firstname, u.lastname
                 FROM {local_iomad_company_users} cu
                 INNER JOIN {user} u ON (cu.userid = u.id)
                 WHERE u.deleted = 0
