cf0bb61340ca0e3ef502c903f1efe92d1942a32b
Derick Turner <derick@e-learndesign.co.uk>
2026-05-11 11:40:04 +0100

    IOMAD: wrong notification class being autoloaded in company - closes #2677

--- local/iomad/lib/company.php
+++ local/iomad/lib/company.php
@@ -56,7 +56,7 @@ use core\event\{
     user_enrolment_created,
     user_deleted,
 };
-use core\output\notification;
+use core\notification;
 use local_iomadcustompage\event\iomadcustompage_deleted;
 
 require_once(dirname(__FILE__) . '/iomad.php');
