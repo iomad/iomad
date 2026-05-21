ee38670c850637683ea63a5af08457d3c1a001f3
Derick Turner <derick@e-learndesign.co.uk>
2026-05-21 11:47:10 +0100

    IOMAD: Enabling an empty time started or time completed throws typeerror exception - closes #2695

--- local/report_users/userdisplay.php
+++ local/report_users/userdisplay.php
@@ -304,6 +304,10 @@ if (!empty($data)) {
         }
         if (!empty($data->timestarted)) {
             foreach ($data->timestarted as $key => $value) {
+                // Check for null values.
+                if (empty($data->origtimestarted[$key])) {
+                    $data->origtimestarted[$key] = 0;
+                }
                 $testtime = strtotime("0:00", $data->origtimestarted[$key]);
                 $senttime = strtotime($value['year'] . "-" . $value['month'] . "-" . $value['day']);
 
@@ -316,6 +320,10 @@ if (!empty($data)) {
         if (!empty($data->timecompleted)) {
             foreach ($data->timecompleted as $key => $value) {
                 if ($trackrec = $DB->get_record('local_iomad_tracks', ['id' => $key])) {
+                    // Check for null values.
+                    if (empty($data->origtimecompleted[$key])) {
+                        $data->origtimecompleted[$key] = 0;
+                    }
                     $testtime = strtotime("0:00", $data->origtimecompleted[$key]);
                     $senttime = strtotime($value['year'] . "-" . $value['month'] . "-" . $value['day']);
 
