af074d6ca766377c735bb4538db9b38804f20626
tom5850 <thomas.schlienger@treesolution.com>
2026-05-24 20:04:12 +0200

    4.5.11 Fix: TypeError during user signup

--- local/iomad_signup/lib.php
+++ local/iomad_signup/lib.php
@@ -94,7 +94,7 @@ function local_iomad_signup_user_created($user) {
 
         // Check if we have a company id from the URL or SESSION.
         $companyid = iomad::get_my_companyid($context, false);
-        if (!empty($companyid)) {
+        if (!empty($companyid) && $companyid > 0) {
             $company = new company($companyid);
             $found = true;
         }
