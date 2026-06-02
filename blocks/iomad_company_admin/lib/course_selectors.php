ab72029ddc30abffe902cdaa2140a6e9742c354a
ericfalcon <42006279+ericfalcon@users.noreply.github.com>
2026-06-03 00:55:50 +0200

    Refactor course selectors for better naming clarity
    Reorder required fields and update fullname formatting to remove spaces in parentheses. Implement prefix_shortname function to distinguish courses with the same name.

--- blocks/iomad_company_admin/lib/course_selectors.php
+++ blocks/iomad_company_admin/lib/course_selectors.php
@@ -33,7 +33,7 @@ abstract class company_course_selector_base extends course_selector_base {
     protected $companycontext;
 
     //overridden to include the sortorder field
-    protected $requiredfields = array('id', 'fullname', 'sortorder', 'shortname');
+    protected $requiredfields = array('id', 'fullname', 'shortname', 'sortorder');
 
     public function __construct($name, $options) {
         $this->companyid  = $options['companyid'];
@@ -63,7 +63,7 @@ abstract class company_course_selector_base extends course_selector_base {
                 $context = context_course::instance($id);
                 if (count_enrolled_users($context) > 0) {
                     $courselist[ $id ]->hasenrollments = true;
-                    $courselist[ $id ]->fullname = $course->fullname . " (" . $strhasenrollments .")";
+                    $courselist[ $id ]->fullname = $course->fullname . "(" . $strhasenrollments .")";
                     $this->hasenrollments = true;
                 }
             }
@@ -74,7 +74,7 @@ abstract class company_course_selector_base extends course_selector_base {
                 if ($companygroup = company::get_company_group($this->companyid, $id)) {
                     if ($DB->get_records('groups_members', array('groupid' => $companygroup->id))) {
                         $courselist[ $id ]->hasenrollments = true;
-                        $courselist[ $id ]->fullname = $course->fullname . " (" . $strsharedhasenrollments .")";
+                        $courselist[ $id ]->fullname = $course->fullname . "(" . $strsharedhasenrollments .")";
                         $this->hasenrollments = true;
                     }
                 }
@@ -91,10 +91,21 @@ abstract class company_course_selector_base extends course_selector_base {
                 $courseid = $DB->get_field('companylicense_users', 'licensecourseid', ['id' => $id]);
             }
             if ($DB->get_record('course', ['id' => $courseid, 'visible' => 0])) {
-                $allcourses[$id]->fullname = $course->fullname . " (" . get_string('hidden', 'badges') . ")";
+                $allcourses[$id]->fullname = $course->fullname . "(" . get_string('hidden', 'badges') . ")";
             }
         }
     }
+
+    // -- MODIFICATION INEMA --
+    // Préfixe chaque entrée du tableau avec [shortname] pour distinguer les cours homonymes entre campus.
+    protected function prefix_shortname(&$courselist) {
+        foreach ($courselist as $id => $course) {
+            if (!empty($course->shortname)) {
+                $courselist[$id]->fullname = '[' . $course->shortname . '] ' . $course->fullname;
+            }
+        }
+    }
+    // -- FIN MODIFICATION INEMA --
 
     protected function process_license_allocations(&$licensecourses, $userid) {
         global $CFG, $DB;
@@ -110,13 +121,6 @@ abstract class company_course_selector_base extends course_selector_base {
                                                               'licensecourseid' => $course->id))) {
                 $licensecourses[$id]->fullname = $course->fullname . '*';
             }
-        }
-    }
-
-    protected function process_shortname(&$allcourses) {
-
-        foreach ($allcourses as $id => $course) {
-            $allcourses[$id]->fullname = $course->fullname . " (" . $course->shortname . ")";
         }
     }
 }
@@ -256,9 +260,11 @@ class current_company_course_selector extends company_course_selector_base {
         }
 
         // Have any of the courses got enrollments?
-        $this->process_shortname($availablecourses);
         $this->process_enrollments($availablecourses);
         $this->process_hidden_courses($availablecourses);
+        // -- MODIFICATION INEMA -- préfixe [shortname] pour distinguer les cours homonymes
+        $this->prefix_shortname($availablecourses);
+        // -- FIN MODIFICATION INEMA --
 
         // Set up empty return.
         $coursearray = array();
@@ -388,7 +394,6 @@ class all_department_course_selector extends company_course_selector_base {
         if (empty($availablecourses) && empty($globalcourses)) {
             return array();
         }
-        $this->process_shortname($availablecourses);
         $this->process_hidden_courses($availablecourses);
 
         // Set up empty return.
@@ -555,9 +560,11 @@ class potential_company_course_selector extends company_course_selector_base {
         }
 
         // Have any of the courses got enrollments?
-        $this->process_shortname($availablecourses);
         $this->process_enrollments($availablecourses);
         $this->process_hidden_courses($availablecourses);
+        // -- MODIFICATION INEMA -- préfixe [shortname] pour distinguer les cours homonymes
+        $this->prefix_shortname($availablecourses);
+        // -- FIN MODIFICATION INEMA --
 
         if ($search) {
             $groupname = get_string('potcoursesmatching', 'block_iomad_company_admin', $search);
@@ -679,7 +686,6 @@ class potential_subdepartment_course_selector extends company_course_selector_ba
         }
 
         // Have any of the courses got enrollments?
-        $this->process_shortname($availablecourses);
         $this->process_enrollments($sanitisedcourses);
         $this->process_hidden_courses($availablecourses);
 
@@ -727,7 +733,6 @@ class any_course_selector extends company_course_selector_base {
         if (empty($availablecourses)) {
             return array();
         }
-        $this->process_shortname($availablecourses);
         $this->process_hidden_courses($availablecourses);
 
         if ($search) {
@@ -808,7 +813,6 @@ class current_user_course_selector extends company_course_selector_base {
                     unset($coursearray[$courseid]);
                 }
             }
-            $this->process_shortname($coursearray);
             $this->process_hidden_courses($coursearray);
 
             return array($groupname => $coursearray);
@@ -966,7 +970,6 @@ class potential_user_course_selector extends company_course_selector_base {
         if (empty($availablecourses)) {
             return array();
         }
-        $this->process_shortname($availablecourses);
         $this->process_hidden_courses($availablecourses);
 
         if ($search) {
@@ -1027,7 +1030,7 @@ class current_user_license_course_selector extends company_course_selector_base 
         $params['userid'] = $this->user->id;
         $params['licenseid'] = $this->licenseid;
 
-        $fields      = 'SELECT clu.id, ' . $this->required_fields_sql('c');
+        $fields      = 'SELECT clu.id, c.fullname ';
         $countfields = 'SELECT COUNT(clu.id)';
 
         $sql = " FROM {course} c,
@@ -1052,7 +1055,6 @@ class current_user_license_course_selector extends company_course_selector_base 
         if (empty($availablecourses)) {
             return array();
         }
-        $this->process_shortname($availablecourses);
         $this->process_license_allocations($availablecourses, $this->user->id);
         $this->process_hidden_courses($availablecourses, true);
 
@@ -1257,7 +1259,6 @@ class potential_user_license_course_selector extends company_course_selector_bas
         if (empty($availablecourses)) {
             return array();
         }
-        $this->process_shortname($availablecourses);
         $this->process_hidden_courses($availablecourses);
 
         if ($search) {
