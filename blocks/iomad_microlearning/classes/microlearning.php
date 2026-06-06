d2154c03da1ac7675783a459a303cb3e524cb1e3
Derick Turner <derick@e-learndesign.co.uk>
2026-06-06 14:54:34 +0100

    IOMAD: 4.5/5.0 Microlearning Cron Job Fails Due to Missing Namespace Imports - fix by @tom5850 - closes #2706

--- blocks/iomad_microlearning/classes/microlearning.php
+++ blocks/iomad_microlearning/classes/microlearning.php
@@ -34,8 +34,11 @@ use block_iomad_microlearning\event\{
     thread_updated,
     thread_schedule_updated,
 };
+use company;
 use context_system;
+use EmailTemplate;
 use html_writer;
+use moodle_url;
 
 /**
  * IOMAD microlearning block class definition
