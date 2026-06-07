490d797a193bfcfcfc075d02398633c3a8357233
Derick Turner <derick@e-learndesign.co.uk>
2026-06-07 14:42:49 +0100

    IOMAD: 4.5E/5.1+ TypeError during user signup: iomad::get_my_companyid() returns stdClass instead of int - fix by @tom5850 - #2704

--- local/iomad/classes/company.php
+++ local/iomad/classes/company.php
@@ -4939,7 +4939,7 @@ class company {
 
             // Check if we have a company id from the URL or SESSION.
             $companyid = iomad::get_my_companyid($context, false);
-            if (!empty($companyid)) {
+            if (!empty($companyid) && $companyid > 0) {
                 $company = new company($companyid);
                 $found = true;
             }
