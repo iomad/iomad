13252709e64e2431ee8c97f1c0ae966e63b3f942
Derick Turner <derick@e-learndesign.co.uk>
2026-06-12 15:52:47 +0100

    IOMAD: Company Manager Loses Capabilities After Editing Child Company - fix proposed by @tom5850 - closes #2727

--- blocks/iomad_company_admin/company_edit_form.php
+++ blocks/iomad_company_admin/company_edit_form.php
@@ -629,8 +629,10 @@ if ($mform->is_cancelled()) {
             );
         }
 
-        // Is the current user in the company?
-        if (company_user::is_company_user()) {
+        // // Only reload if the current user is editing their own company, not a child company they manage.
+        if (company_user::is_company_user() &&
+            isset($USER->company->id) &&
+            $USER->company->id == $companyid) {
             company_user::reload_company();
         }
     }
