8fe6326b9b5dd91bd80c7e8c441bddd04514885f
Derick Turner <derick@e-learndesign.co.uk>
2026-06-12 17:20:59 +0100

    IOMAD: Incorrect companyid in local_iomad_track Table for Bulk Enrollments with new ad hoc task - closes #2725

--- public/local/iomad/classes/company_user.php
+++ public/local/iomad/classes/company_user.php
@@ -651,6 +651,7 @@ class company_user {
                             'courseid' => $courseid,
                             'context' => context_course::instance($courseid),
                             'relateduserid' => $user->id,
+                            'companyid' => $companyid,
                             'other' => ['enrol' => 'manual'],
                         ]
                     );
