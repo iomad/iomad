eac71d0c589735de4fe8b6f066e2105dba89e343
Derick Turner <derick@e-learndesign.co.uk>
2026-06-12 13:21:34 +0100

    IOMAD: block_mycourses: show "Available courses" tab by default when the user has no in-progress courses - improvement suggested by @ccomincini  #2720

--- blocks/iomad_mycourses/classes/output/main.php
+++ blocks/iomad_mycourses/classes/output/main.php
@@ -130,6 +130,12 @@ class main implements renderable, templatable {
             $viewinginprogress = true;
         }
 
+        // If the user has no in-progress courses, show the "Available courses" tab
+        // instead of an empty "Courses in progress" tab.
+        if ($tab == 'inprogress' && empty($myinprogress) && !empty($myavailable)) {
+            $tab = 'available';
+        }
+
         // Set the default for no courses.
         $nocoursesurl = $output->image_url('courses', 'block_iomad_mycourses')->out();
 
