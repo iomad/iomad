337c8db0967a9fcc1464048fb804bcc33096a935
Derick Turner <derick@e-learndesign.co.uk>
2026-06-23 11:42:45 +0100

    IOMAD: 4.5E+ Certificate download links in reports and My Courses block are using the incorrect component and filearea - closes #2736

--- local/report_users/classes/tables/completion_table.php
+++ local/report_users/classes/tables/completion_table.php
@@ -439,7 +439,7 @@ class completion_table extends table_sql {
                         $certurl = moodle_url::make_file_url(
                             '/pluginfile.php',
                             '/' . $usercontext->id .
-                            '/local_iomad_track/issue/' . $traccertrec->trackid .
+                            '/local_iomad/certificate_issue/' . $traccertrec->trackid .
                             '/' . $traccertrec->filename);
                         $returntext .= html_writer::start_tag(
                             'a',
