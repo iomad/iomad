70432c4b7449585ca7d87121b2281fbfaa86c533
Derick Turner <derick@e-learndesign.co.uk>
2026-06-24 13:23:56 +0100

    IOMAD: Add additional info to training events which can be added to email templates - training event activity 4.5+ #2741

--- mod/trainingevent/lib.php
+++ mod/trainingevent/lib.php
@@ -63,7 +63,14 @@ function trainingevent_add_instance($trainingevent) {
 
     $trainingevent->name = get_trainingevent_name($trainingevent);
     $trainingevent->timemodified = time();
+
+    // Deal with the Additional info HTML field.
+    $trainingevent->additionalinfo = '';
+    if (!empty($trainingevent->requireadditionalinfo)) {
+        $trainingevent->additionalinfo = $trainingevent->additionalinfo_editor['text'];
+    }
     $trainingevent->id = $DB->insert_record("trainingevent", $trainingevent);
+
     grade_update('mod/trainingevent',
                  $trainingevent->course,
                  'mod',
@@ -108,6 +115,12 @@ function trainingevent_update_instance($trainingevent) {
 
     if (empty($trainingevent->requirenotes)) {
         $trainingevent->requirenotes = 0;
+    }
+
+    // Deal with the Additional info HTML field.
+    $trainingevent->additionalinfo = '';
+    if (!empty($trainingevent->requireadditionalinfo)) {
+        $trainingevent->additionalinfo = $trainingevent->additionalinfo_editor['text'];
     }
 
     grade_update('mod/trainingevent',
@@ -512,6 +525,9 @@ function trainingevent_user_attending($event) {
     // Set the location time.
     $location->time = userdate($trainingevent->startdatetime, $CFG->iomad_date_format . " %I:%M%p");
 
+    // Add any additional information.
+    $location->additionalinfo = $trainingevent->additionalinfo;
+
     // Is it only onto the waiting list?
     if ($sendemails &&
         !empty($event->other['waitlisted'])) {
@@ -662,6 +678,9 @@ function trainingevent_user_removed($event) {
 
     // Set the company.
     $company = new company($event->companyid);
+
+    // Add any additional information.
+    $location->additionalinfo = $trainingevent->additionalinfo;
 
     // Send an email as long as it hasn't already started.
     if ($trainingevent->startdatetime > $event->timecreated) {
@@ -831,6 +850,10 @@ function trainingevent_attendance_changed($event) {
     // Add the time to the location object.
     $location->time = userdate($trainingevent->startdatetime, $CFG->iomad_date_format . " %I:%M%p");
     $chosenlocation->time = userdate($chosenevent->startdatetime, $CFG->iomad_date_format . " %I:%M%p");
+
+    // Add any additional information.
+    $chosenlocation->additionalinfo = $chosenevent->additionalinfo;
+    $location->additionalinfo = $trainingevent->additionalinfo;
 
     // Get the course teachers using using groups if required.
     $usergroups = groups_get_user_groups($course->id, $user->id);
