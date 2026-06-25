eec058919c1360af9b7604cb4b845f6944022d86
Derick Turner <derick@e-learndesign.co.uk>
2026-06-25 17:05:16 +0100

    IOMAD: Add additional info to training events which can be added to email templates - 4.5E+ email vars and language strings - #2741

--- public/local/iomad/classes/emailvars.php
+++ public/local/iomad/classes/emailvars.php
@@ -146,7 +146,7 @@ class emailvars {
                         'Course_FullName', 'Course_ShortName', 'CourseURL', 'Course_ReportText',
             // ClassRoom fields.
                         'Classroom_Name', 'Classroom_Address', 'Classroom_Postcode', 'Classroom_City',
-                        'Classroom_Country', 'Classroom_Capacity', 'Classroom_Summary', 'Classroom_Time',
+                        'Classroom_Country', 'Classroom_Capacity', 'Classroom_Summary', 'Classroom_Time', 'Classroom_Additionalinfo',
             // Site fields.
                         'Site_FullName', 'Site_ShortName', 'Site_Summary',
             // Company fields.
