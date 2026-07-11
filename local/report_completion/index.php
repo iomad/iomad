151c1a32a529ab5988aaa9d83678f12e3fd1fe89
Derick Turner <derick@e-learndesign.co.uk>
2026-07-11 12:42:27 +0100

    IOMAD: Per-course completion report does not display the selected course name - closes #2745

--- local/report_completion/index.php
+++ local/report_completion/index.php
@@ -1244,6 +1244,11 @@ if (empty($courseid)) {
     if (!$table->is_downloading()) {
         echo $output->header();
 
+        // Display the page heading.
+        echo html_writer::start_tag('div', ['class' => 'iomad_report_heading_wraper']);
+        echo html_writer::tag('span', $heading, ['class' => 'iomad_report_heading']);
+        echo html_writer::end_tag('div');
+
         // Display the search form and department picker.
         if (!empty($companyid)) {
             if (empty($table->is_downloading())) {
