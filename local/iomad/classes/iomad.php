f791bac8c10d7c3149d6af7a4309a44d70152c3e
Derick Turner <derick@e-learndesign.co.uk>
2026-07-15 16:48:29 +0100

    IOMAD: need default value for get_company_postfix() - #2754

--- local/iomad/classes/iomad.php
+++ local/iomad/classes/iomad.php
@@ -308,7 +308,7 @@ class iomad {
      * @param string $separator
      * @return string
      */
-    public static function get_company_postfix(string $separator): string {
+    public static function get_company_postfix(?string $separator = '_'): string {
 
         // Set the default blank.
         $postfix = "";
