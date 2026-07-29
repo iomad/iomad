fca22a21555198af24e661a3522df09609416451
Derick Turner <derick@e-learndesign.co.uk>
2026-07-29 11:47:55 +0100

    IOMAD: copanyid variable no longer present after changes - #2754

--- public/local/report_completion_overview/settings.php
+++ public/local/report_completion_overview/settings.php
@@ -43,7 +43,7 @@ if ($hassiteconfig && !empty($USER->id)) {
         30 * 24 * 60 * 60)
     );
 
-    if (!empty($postfix)) {  // Requires that $companyid > 0.
+    if (!empty($postfix)) {
         $settings->add(new admin_setting_configduration(
             'local_report_completion_overview/warningduration' . $postfix,
             get_string('warningdurationcompany', 'local_report_completion_overview'),
