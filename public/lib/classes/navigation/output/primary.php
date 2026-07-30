Prodigy-MISA-groupe3/eLEARNING_deploy

commit b970beea309a44a8bfb5666ef67d16c39d0a9679

IOMAD: updated code to reflect the table name changes from the previous commits - #2567

public/lib/classes/navigation/output/primary.php
@@ -118,7 +118,7 @@ class primary implements renderable, templatable {
         // Deal with company custom menu items.
         $custommenuitems = $CFG->custommenuitems;
         if ($companyid = iomad::get_my_companyid(\context_system::instance(), false)) {
-            if ($companyrec = $DB->get_record('company', array('id' => $companyid))) {
+            if ($companyrec = $DB->get_record('local_iomad_companies', array('id' => $companyid))) {
                 if (!empty($companyrec->custommenuitems)) {
                     $custommenuitems = $companyrec->custommenuitems;
                 }

iomad/iomad

commit b24bde6d26d06e2da10c0fec87fd037ee007c542

IOMAD: 5.1 syntax error introduced in b970beea309a44a8bfb5666ef67d16c39d0a9679

public/lib/classes/navigation/output/primary.php
@@ -137,7 +137,7 @@ class primary implements renderable, templatable {
         }
 
         // If filtering of the primary custom menu is enabled, apply only the string filters.
-        if (!empty($CFG->navfilter && !empty($CFG->stringfilters))) {
+        if (!empty($CFG->navfilter) && !empty($CFG->stringfilters)) {
             // Apply filters that are enabled for Content and Headings.
             $filtermanager = \filter_manager::instance();
             $custommenuitems = $filtermanager->filter_string($custommenuitems, \context_system::instance());