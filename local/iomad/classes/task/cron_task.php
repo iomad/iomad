be1acdc33ef6ec605144fed886b6bb3e677e220b
Derick Turner <derick@e-learndesign.co.uk>
2026-08-03 13:12:46 +0100

    IOMAD: Undefined licensid property prevents deletion of unused allocations for expired licenses - closes #2767

--- local/iomad/classes/task/cron_task.php
+++ local/iomad/classes/task/cron_task.php
@@ -220,7 +220,7 @@ class cron_task extends scheduled_task {
                             }
                         } else {
                             mtrace("Removing unused license for userid $litrec->userid from courseid $litrec->courseid");
-                            $DB->delete_records('companylicense_users', ['licenseid' => $litrec->licensid,
+                            $DB->delete_records('companylicense_users', ['licenseid' => $litrec->licenseid,
                                                                          'licensecourseid' => $litrec->courseid,
                                                                          'userid' => $litrec->userid,
                                                                          'issuedate' => $litrec->licenseallocated]);
