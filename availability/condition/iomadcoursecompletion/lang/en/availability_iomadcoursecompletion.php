29677ceafbffb9340f00e9f0beaa99b858a0b56d
Derick Turner <derick@e-learndesign.co.uk>
2026-08-25 16:40:03 +0100

    IOMAD: Create new "Previously completed" availability condition which checks to see if a user has already completed a course in the IOMAD reports - closes #2738

---
+++ availability/condition/iomadcoursecompletion/lang/en/availability_iomadcoursecompletion.php
@@ -0,0 +1,41 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Language strings.
+ *
+ * @package availability_iomadcoursecompletion
+ * @copyright 2026 e-Learn Design Ltd. https://www.e-learndesign.co.uk
+ * @author Derick Turner
+ * @license http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['anycourse'] = '(Any course)';
+$string['description'] = 'Allow only users who have previously completed a specified course, or allow users who have previously completed all courses.';
+$string['error_selectcourse'] = 'You must select a course.';
+$string['missing'] = '(Missing course)';
+$string['pluginname'] = 'Restriction by previously completed course';
+$string['privacy:metadata'] = 'The \'Restriction by previously completed course\' plugin does not store any personal data.';
+$string['requires_anycourse'] = 'You must have previously completed any course';
+$string['requires_anycoursevalid'] = 'You must have previously completed any course and training has not expired';
+$string['requires_course'] = 'You must have previously completed the course \'<strong>{$a}</strong>\'';
+$string['requires_coursevalid'] = 'The course \'<strong>{$a}</strong>\' is complete and not expired';
+$string['requires_notanycourse'] = 'You have not previously completed any course';
+$string['requires_notanycoursevalid'] = 'You have not previously completed any course with in-date training';
+$string['requires_notcourse'] = 'The course \'<strong>{$a}</strong>\' is not complete';
+$string['requires_notcoursevalid'] = 'The course \'<strong>{$a}</strong>\' is not complete or has expired';
+$string['title'] = 'Previously completed course';
+$string['validoption'] = 'Training has not expired';
