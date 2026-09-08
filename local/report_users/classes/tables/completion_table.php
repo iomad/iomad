2964ffbd3be69cccad4dc0b33b2fac57dccd5913
Derick Turner <derick@e-learndesign.co.uk>
2026-09-08 11:18:12 +0100

    IOMAD: Status progress bar hides the completion percentage for low values - closes #2746

--- local/report_users/classes/tables/completion_table.php
+++ local/report_users/classes/tables/completion_table.php
@@ -561,6 +561,12 @@ class completion_table extends table_sql {
                             return html_writer::start_tag(
                                 'div',
                                 [
+                                    'class' => 'row',
+                                ]
+                            ) .
+                            html_writer::start_tag(
+                                'div',
+                                [
                                     'class' => 'progress',
                                     'style' => 'height:20px',
                                     'data-html' => 'true',
@@ -569,12 +575,14 @@ class completion_table extends table_sql {
                             ) .
                             html_writer::tag(
                                 'div',
-                                '0%',
+                                '',
                                 [
                                     'class' => 'progress-bar',
                                     'style' => 'width:0%;height:20px',
                                 ]
                             ) .
+                            html_writer::end_tag('div') .
+                            "&nbsp" . get_string('percents', 'moodle', 0) .
                             html_writer::end_tag('div');
                         } else {
                             return get_string('completion-alt-auto-y', 'completion', "0%");
@@ -601,6 +609,12 @@ class completion_table extends table_sql {
                 return html_writer::start_tag(
                                 'div',
                                 [
+                                    'class' => 'row',
+                                ]
+                            ) .
+                            html_writer::start_tag(
+                                'div',
+                                [
                                     'class' => 'progress',
                                     'style' => 'height:20px',
                                     'data-html' => 'true',
@@ -609,12 +623,14 @@ class completion_table extends table_sql {
                             ) .
                             html_writer::tag(
                                 'div',
-                                $progress . '%',
+                                '',
                                 [
                                     'class' => 'progress-bar',
                                     'style' => 'width:' . $progress . '%;height:20px',
                                 ]
                             ) .
+                            html_writer::end_tag('div') .
+                            "&nbsp" . get_string('percents', 'moodle', $progress) .
                             html_writer::end_tag('div');
             } else {
                 return get_string('completion-alt-auto-y', 'completion', "$progress%") ."\r\n$tooltip";
