a8a643145595c10461f8a164ad44427d294efb52
Derick Turner <derick@e-learndesign.co.uk>
2026-09-09 12:56:36 +0100

    IOMAD: ogin/signup_form.php still using deprecated class reference - closes #2796

--- public/login/signup_form.php
+++ public/login/signup_form.php
@@ -26,6 +26,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 use local_iomad\company;
+use local_iomad\company_user;
 
 require_once($CFG->libdir.'/formslib.php');
 require_once($CFG->dirroot.'/user/profile/lib.php');
